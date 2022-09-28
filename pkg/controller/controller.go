10e7bba3c0b9ad53543243283ed4d988baaec896
Samu <veloso.lopez.samuel@gmail.com>
2022-09-28 13:30:33 +0200

    Implement secrets generator controller (#174)

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -4,9 +4,22 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 
 	"github.com/kubeflow/kfctl/v3/pkg/controller/kfdef"
+	"github.com/kubeflow/kfctl/v3/pkg/controller/secretgenerator"
 )
 
 // AddToManager adds all Controllers to the Manager
 func AddToManager(m manager.Manager) error {
-	return kfdef.AddToManager(m)
+	// Add KfDef controller
+	err := kfdef.AddToManager(m)
+	if err != nil {
+		return err
+	}
+
+	// Add Secrets Generator controller
+	err = secretgenerator.AddToManager(m)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
