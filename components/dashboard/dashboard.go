097a2be53189b62bbd27f38da24cdbf3ef769ffe
Wen Zhou <wenzhou@redhat.com>
2023-09-12 08:27:19 +0200

    Update path for downstream dashboard image to use
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- components/dashboard/dashboard.go
+++ components/dashboard/dashboard.go
@@ -128,9 +128,9 @@ func (d *Dashboard) ReconcileComponent(owner metav1.Object, cli client.Client, s
 			}
 		}
 
-		// Update image parameters
+		// Update image parameters (ODH does not use this solution, only downstream)
 		if dscispec.DevFlags.ManifestsUri == "" {
-			if err := deploy.ApplyImageParams(Path, imageParamMap); err != nil {
+			if err := deploy.ApplyImageParams(PathSupported, imageParamMap); err != nil {
 				return err
 			}
 		}
