69d58193013361c05bbd78b45163f795fd056404
Wen Zhou <wenzhou@redhat.com>
2023-09-14 10:48:49 +0200

    Fix: path for kserve
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- components/kserve/kserve.go
+++ components/kserve/kserve.go
@@ -15,7 +15,7 @@ import (
 
 const (
 	ComponentName          = "kserve"
-	Path                   = deploy.DefaultManifestPath + "/" + ComponentName + "/config/overlays/odh"
+	Path                   = deploy.DefaultManifestPath + "/" + ComponentName + "/overlays/odh"
 	DependentComponentName = "odh-model-controller"
 	DependentPath          = deploy.DefaultManifestPath + "/" + DependentComponentName + "/base"
 	ServiceMeshOperator    = "servicemeshoperator"
