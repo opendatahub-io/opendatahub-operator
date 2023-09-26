84b42ebfc3bec5b69d971868ab7cbb7d54c20c07
Wen Zhou <wenzhou@redhat.com>
2023-09-26 07:27:36 +0200

    Update components/dashboard/dashboard.go

--- components/dashboard/dashboard.go
+++ components/dashboard/dashboard.go
@@ -71,10 +71,11 @@ func (d *Dashboard) ReconcileComponent(cli client.Client, owner metav1.Object, d
 			}
 
 			// Deploy CRDs for odh-dashboard
-			err = deploy.DeployManifestsFromPath(owner, cli, ComponentName,
+			err = deploy.DeployManifestsFromPath(cli, owner,,
 				PathCRDs,
 				dscispec.ApplicationsNamespace,
-				cli.Scheme(), enabled)
+				ComponentName,
+				enabled)
 			if err != nil {
 				return fmt.Errorf("failed to deploy dashboard crds %s: %v", PathCRDs, err)
 			}
