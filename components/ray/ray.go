70689b17f1d6cb2afdf6daa097240f51ebfe8585
Wen Zhou <wenzhou@redhat.com>
2023-10-20 07:19:39 +0200

    update(ray): new path to manifests (#640)
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- components/ray/ray.go
+++ components/ray/ray.go
@@ -15,7 +15,7 @@ import (
 
 var (
 	ComponentName = "ray"
-	RayPath       = deploy.DefaultManifestPath + "/" + "ray/operator/base"
+	RayPath       = deploy.DefaultManifestPath + "/ray/openshift"
 )
 
 type Ray struct {
