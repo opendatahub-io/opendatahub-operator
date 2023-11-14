opendatahub-io/opendatahub-operator

commit d0e6c9816f24234b1904d84ae379cf5472835b1f

Update Dockerfile and Makefile

Makefile
@@ -4,6 +4,12 @@
 # - use the VERSION as arg of the bundle target (e.g make bundle VERSION=0.0.2)
 # - use environment variables to overwrite this value (e.g export VERSION=0.0.2)
 VERSION ?= 0.0.1
+# TOD0: Revert default image repo to `opendatahub` when merging to main branch
+IMG ?= quay.io/vhire/opendatahub-operator:dev-$(VERSION)
+IMAGE_BUILDER ?= podman
+OPERATOR_NAMESPACE ?= opendatahub-operator-system
+CHANNELS="stable,rolling"
+DEFAULT_CHANNEL="rolling"
 
 # CHANNELS define the bundle channels used in the bundle.
 # Add a new line here if you would like to change its default config. (E.g CHANNELS = "candidate,fast,stable")
@@ -117,11 +123,14 @@ run: manifests generate fmt vet ## Run a controller from your host.
 
 .PHONY: docker-build
 docker-build: test ## Build docker image with the manager.
-	docker build -t ${IMG} .
+	${IMAGE_BUILDER} build -t ${IMG} .
 
 .PHONY: docker-push
 docker-push: ## Push docker image with the manager.
-	docker push ${IMG}
+	${IMAGE_BUILDER} push ${IMG}
+
+.PHONY: image
+image: docker-build docker-push ## Build and push docker image with the manager.
 
 ##@ Deployment
 
@@ -187,7 +196,7 @@ bundle: manifests kustomize ## Generate bundle manifests and metadata, then vali
 
 .PHONY: bundle-build
 bundle-build: ## Build the bundle image.
-	docker build -f bundle.Dockerfile -t $(BUNDLE_IMG) .
+	${IMAGE_BUILDER} build -f bundle.Dockerfile -t $(BUNDLE_IMG) .
 
 .PHONY: bundle-push
 bundle-push: ## Push the bundle image.
@@ -227,7 +236,7 @@ endif
 # https://github.com/operator-framework/community-operators/blob/7f1438c/docs/packaging-operator.md#updating-your-existing-operator
 .PHONY: catalog-build
 catalog-build: opm ## Build a catalog image.
-	$(OPM) index add --container-tool docker --mode semver --tag $(CATALOG_IMG) --bundles $(BUNDLE_IMGS) $(FROM_INDEX_OPT)
+	$(OPM) index add --container-tool ${IMAGE_BUILDER} --mode semver --tag $(CATALOG_IMG) --bundles $(BUNDLE_IMGS) $(FROM_INDEX_OPT)
 
 # Push the catalog image.
 .PHONY: catalog-push

Dockerfile
@@ -1,7 +1,9 @@
 # Build the manager binary
-FROM golang:1.18 as builder
+ARG GOLANG_VERSION=1.18.9
+FROM registry.access.redhat.com/ubi8/go-toolset:$GOLANG_VERSION as builder
 
 WORKDIR /workspace
+USER root
 # Copy the Go Modules manifests
 COPY go.mod go.mod
 COPY go.sum go.sum
@@ -17,9 +19,8 @@ COPY controllers/ controllers/
 # Build
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o manager main.go
 
-# Use distroless as minimal base image to package the manager binary
-# Refer to https://github.com/GoogleContainerTools/distroless for more details
-FROM gcr.io/distroless/static:nonroot
+
+FROM registry.access.redhat.com/ubi8/ubi-minimal:latest
 WORKDIR /
 COPY --from=builder /workspace/manager .
 USER 65532:65532

commit f5056173f9d5e492a67fba17becf382ef62bf615

Makefile: change always true conditions
After commit
d0e6c9816f24 ("Update Dockerfile and Makefile")
which defines CHANNELS and DEFAULT_CHANNEL variables, conditions like
`ifneq ($(origin CHANNELS), undefined)` are always true and
misleading. Change it to check for blank since it still can be
overwritten from command line.

Change also CATALOG_BASE_IMG. It was not affected by the old patch
but looks like makes the same sense.

Related: #696

Fixes: d0e6c9816f24 ("Update Dockerfile and Makefile")

Signed-off-by: Yauheni Kaliuta <ykaliuta@redhat.com>

Makefile
@@ -27,7 +27,7 @@ CHANNELS="fast"
 # To re-generate a bundle for other specific channels without changing the standard setup, you can:
 # - use the CHANNELS as arg of the bundle target (e.g make bundle CHANNELS=candidate,fast,stable)
 # - use environment variables to overwrite this value (e.g export CHANNELS="candidate,fast,stable")
-ifneq ($(origin CHANNELS), undefined)
+ifneq ($(CHANNELS),)
 BUNDLE_CHANNELS := --channels=$(CHANNELS)
 endif
 
@@ -37,7 +37,7 @@ DEFAULT_CHANNEL="fast"
 # To re-generate a bundle for any other default channel without changing the default setup, you can:
 # - use the DEFAULT_CHANNEL as arg of the bundle target (e.g make bundle DEFAULT_CHANNEL=stable)
 # - use environment variables to overwrite this value (e.g export DEFAULT_CHANNEL="stable")
-ifneq ($(origin DEFAULT_CHANNEL), undefined)
+ifneq ($(DEFAULT_CHANNEL),)
 BUNDLE_DEFAULT_CHANNEL := --default-channel=$(DEFAULT_CHANNEL)
 endif
 
@@ -271,7 +271,7 @@ BUNDLE_IMGS ?= $(BUNDLE_IMG)
 CATALOG_IMG ?= $(IMAGE_TAG_BASE)-catalog:v$(VERSION)
 
 # Set CATALOG_BASE_IMG to an existing catalog image tag to add $BUNDLE_IMGS to that image.
-ifneq ($(origin CATALOG_BASE_IMG), undefined)
+ifneq ($(CATALOG_BASE_IMG),)
 FROM_INDEX_OPT := --from-index $(CATALOG_BASE_IMG)
 endif
 