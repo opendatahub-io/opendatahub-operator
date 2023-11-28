16c9a171ddf0916d9e814243348776c1fa8196e9
bartoszmajsak <bartosz.majsak@gmail.com>
2023-11-28 19:13:25 +0100

    chore: renames istio resources to use opendatahub prefix instead of odh

--- pkg/feature/servicemesh/manifests_config.go
+++ pkg/feature/servicemesh/manifests_config.go
@@ -12,11 +12,11 @@ const (
 )
 
 // OverwriteIstioGatewayVar replaces the ISTIO_GATEWAY with given namespace and "odh-gateway" in the specified ossm.env file.
-// This is used in conjunction with kustomize overlays for Kubeflow notebook controllers. By overwritting referenced we can set
+// This is used in conjunction with kustomize overlays for Kubeflow notebook controllers. By overwriting referenced we can set
 // proper values for environment variables populated through Kustomize.
 func OverwriteIstioGatewayVar(namespace, path string) error {
 	envFile := filepath.Join(path, "ossm.env")
-	replacement := fmt.Sprintf("ISTIO_GATEWAY=%s", namespace+"/odh-gateway")
+	replacement := fmt.Sprintf("ISTIO_GATEWAY=%s", namespace+"/opendatahub-gateway")
 
 	return common.ReplaceInFile(envFile, map[string]string{gatewayPattern: replacement})
 }
