9c9f8e602c8edba6a3b01ae83310c534dd079595
Edgar Hernández <23639005+israel-hdez@users.noreply.github.com>
2024-01-23 14:49:51 -0600

    Patch to enable Secret filtering in KNative Serving
    This prevents KNative's net-istio pods from being OOMKilled in environments with a large number of Secrets.
    
    This would patch a Managed installation. Custom installations would be left untouched.
    
    Signed-off-by: Edgar Hernández <23639005+israel-hdez@users.noreply.github.com>

--- components/kserve/serverless_setup.go
+++ components/kserve/serverless_setup.go
@@ -6,12 +6,10 @@ import (
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/feature"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/feature/serverless"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/feature/servicemesh"
-	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/gvr"
 )
 
 const (
-	knativeServingNamespace = "knative-serving"
-	templatesDir            = "templates/serverless"
+	templatesDir = "templates/serverless"
 )
 
 func (k *Kserve) configureServerlessFeatures() feature.DefinedFeatures {
@@ -27,10 +25,7 @@ func (k *Kserve) configureServerlessFeatures() feature.DefinedFeatures {
 				serverless.EnsureServerlessOperatorInstalled,
 				serverless.EnsureServerlessAbsent,
 				servicemesh.EnsureServiceMeshInstalled,
-				feature.CreateNamespaceIfNotExists(knativeServingNamespace),
-			).
-			PostConditions(
-				feature.WaitForPodsToBeReady(knativeServingNamespace),
+				feature.CreateNamespaceIfNotExists(serverless.KnativeServingNamespace),
 			).
 			Load()
 		if err != nil {
@@ -38,12 +33,27 @@ func (k *Kserve) configureServerlessFeatures() feature.DefinedFeatures {
 		}
 		initializer.Features = append(initializer.Features, servingDeployment)
 
+		servingNetIstioSecretFiltering, err := feature.CreateFeature("serverless-net-istio-secret-filtering").
+			With(initializer.DSCInitializationSpec).
+			From(initializer.Source).
+			Manifests(
+				path.Join(templatesDir, "serving-net-istio-secret-filtering.patch.tmpl"),
+			).
+			WithData(PopulateComponentSettings(k)).
+			PreConditions(serverless.EnsureServerlessServingDeployed).
+			PostConditions(
+				feature.WaitForPodsToBeReady(serverless.KnativeServingNamespace),
+			).
+			Load()
+		if err != nil {
+			return err
+		}
+		initializer.Features = append(initializer.Features, servingNetIstioSecretFiltering)
+
 		servingIstioGateways, err := feature.CreateFeature("serverless-serving-gateways").
 			With(initializer.DSCInitializationSpec).
 			From(initializer.Source).
-			PreConditions(
-				// Check serverless is installed
-				feature.WaitForResourceToBeCreated(knativeServingNamespace, gvr.KnativeServing)).
+			PreConditions(serverless.EnsureServerlessServingDeployed).
 			WithData(
 				serverless.ServingDefaultValues,
 				serverless.ServingIngressDomain,
