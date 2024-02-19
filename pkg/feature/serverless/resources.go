c54172550b39daee548b9d13dea18c84bf1a3c6c
Bartosz Majsak <bartosz.majsak@gmail.com>
2024-02-19 09:03:06 +0100

    fix: improves error message

--- pkg/feature/serverless/resources.go
+++ pkg/feature/serverless/resources.go
@@ -24,7 +24,7 @@ func GetDomain(c client.Client) (string, error) {
 		Namespace: "",
 		Name:      "cluster",
 	}, ingress); err != nil {
-		return "", fmt.Errorf("failed fetching cluster: %w", err)
+		return "", fmt.Errorf("failed fetching cluster's ingress details: %w", err)
 	}
 
 	domain, found, err := unstructured.NestedString(ingress.Object, "spec", "domain")
