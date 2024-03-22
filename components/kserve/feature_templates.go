dc86af0a33d9967ead442747445760aea9098723
Cameron Garrison <cgarriso@redhat.com>
2024-03-22 15:16:37 -0400

    Update components/kserve/feature_templates.go
    Co-authored-by: Bartosz Majsak <bartosz.majsak@gmail.com>

--- components/kserve/feature_templates.go
+++ components/kserve/feature_templates.go
@@ -2,15 +2,27 @@ package kserve
 
 import (
 	"embed"
+	"io/fs"
 	"path"
 )
 
 //go:embed templates
 var kserveEmbeddedFS embed.FS
 
-var (
-	baseDir        = "templates"
-	serviceMeshDir = path.Join(baseDir, "servicemesh")
-	installDir     = path.Join(baseDir, "serving-install")
-	gatewaysDir    = path.Join(baseDir, "serving-istio-gateways")
-)
+const baseDir = "templates"
+
+var Templates = struct {
+	// ServiceMeshDir is the path to the Service Mesh templates.
+	ServiceMeshDir string
+	// InstallDir is the path to the Serving install templates.
+	InstallDir string
+	// GatewaysDir is the path to the Serving Istio gateways templates.
+	GatewaysDir string
+	// Files the templates to be used
+	Files fs.FS
+}{
+	ServiceMeshDir: path.Join(baseDir, "servicemesh"),
+	InstallDir:     path.Join(baseDir, "serving-install"),
+	GatewaysDir:    path.Join(baseDir, "serving-istio-gateways"),
+	Files:         kserveEmbeddedFS,
+}
