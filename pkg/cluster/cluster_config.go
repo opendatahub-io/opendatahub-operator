6bfc2d994b08dda4978822f0396ea9c33afd5406
Wen Zhou <wenzhou@redhat.com>
2024-03-27 19:40:01 +0100

    chore: split upgrade.go into 3 files (#939)
    * chore: split upgrade.go into 3 files
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    
    * update: code review
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    
    * revert: keep return error for the case of readfile etc
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    
    * Update pkg/cluster/cluster_config.go
    
    Co-authored-by: Bartosz Majsak <bartosz.majsak@gmail.com>
    
    ---------
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    Co-authored-by: Bartosz Majsak <bartosz.majsak@gmail.com>

--- pkg/cluster/cluster_config.go
+++ pkg/cluster/cluster_config.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"os"
 
+	ofapi "github.com/operator-framework/api/pkg/operators/v1alpha1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -28,4 +30,27 @@ func GetDomain(c client.Client) (string, error) {
 	}
 
 	return domain, err
+}
+
+func GetOperatorNamespace() (string, error) {
+	data, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+	return string(data), err
+}
+
+// GetClusterServiceVersion retries the clusterserviceversions available in the operator namespace.
+func GetClusterServiceVersion(ctx context.Context, c client.Client, watchNameSpace string) (*ofapi.ClusterServiceVersion, error) {
+	clusterServiceVersionList := &ofapi.ClusterServiceVersionList{}
+	if err := c.List(ctx, clusterServiceVersionList, client.InNamespace(watchNameSpace)); err != nil {
+		return nil, fmt.Errorf("failed listign cluster service versions: %w", err)
+	}
+
+	for _, csv := range clusterServiceVersionList.Items {
+		for _, operatorCR := range csv.Spec.CustomResourceDefinitions.Owned {
+			if operatorCR.Kind == "DataScienceCluster" {
+				return &csv, nil
+			}
+		}
+	}
+
+	return nil, nil
 }
