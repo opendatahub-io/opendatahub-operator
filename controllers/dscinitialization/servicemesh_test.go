0a32786b49bdb69402bd1647a9eed82921db0990
Cameron Garrison <cgarriso@redhat.com>
2024-04-09 13:18:16 -0400

    Update controllers/dscinitialization/servicemesh_test.go
    Co-authored-by: Bartosz Majsak <bartosz.majsak@gmail.com>

--- controllers/dscinitialization/servicemesh_test.go
+++ controllers/dscinitialization/servicemesh_test.go
@@ -11,23 +11,23 @@ import (
 
 var defaultAudiences = []string{"https://kubernetes.default.svc"}
 
-var _ = Describe("Audiences", func() {
-	Describe("Determining if provided audiences are default", func() {
-		Context("When no specific audiences are provided", func() {
+var _ = Describe("Handling Token Audiences", func() {
+	Context("Determining if provided audiences are default", func() {
+		When("no specific audiences are provided", func() {
 			It("should consider the audiences to be default", func() {
 				var specAudiences *[]string
 				Expect(dsci.IsDefaultAudiences(specAudiences)).To(BeTrue())
 			})
 		})
 
-		Context("When the default audiences are provided", func() {
+		When("the default audiences are provided", func() {
 			It("should consider these audiences to be default", func() {
 				specAudiences := &defaultAudiences
 				Expect(dsci.IsDefaultAudiences(specAudiences)).To(BeTrue())
 			})
 		})
 
-		Context("When non-default audiences are provided", func() {
+		When("non-default audiences are provided", func() {
 			It("should not consider these audiences to be default", func() {
 				specAudiences := &[]string{"https://example.com"}
 				Expect(dsci.IsDefaultAudiences(specAudiences)).To(BeFalse())
@@ -35,8 +35,8 @@ var _ = Describe("Audiences", func() {
 		})
 	})
 
-	Describe("Determining the effective cluster audiences", func() {
-		Context("When non-default audiences are provided", func() {
+	Context("Determining the effective cluster audiences", func() {
+		When("non-default audiences are provided", func() {
 			It("should return the provided audiences", func() {
 				specAudiences := &[]string{"https://example.com"}
 				Expect(dsci.GetEffectiveClusterAudiences(specAudiences, nil, logr.Logger{})).To(Equal(*specAudiences))
