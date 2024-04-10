9bb75de8de5ba1c2a8d5686e7205e84eb24e9ee7
Cameron Garrison <cgarriso@redhat.com>
2024-04-10 11:40:18 -0400

    remove fakeClient, stub not mock

--- pkg/cluster/audiences_test.go
+++ pkg/cluster/audiences_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/go-logr/logr"
 	authentication "k8s.io/api/authentication/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/cluster"
 
@@ -18,11 +17,9 @@ import (
 var defaultAudiences = []string{"https://kubernetes.default.svc"}
 
 var _ = Describe("Handling Token Audiences", func() {
-	var fakeClient client.Client
 	var logger logr.Logger
 
 	BeforeEach(func() {
-		fakeClient = fake.NewClientBuilder().WithScheme(testScheme).Build()
 		logger = logr.Logger{}
 	})
 
@@ -30,46 +27,46 @@ var _ = Describe("Handling Token Audiences", func() {
 		When("non-default audiences are provided", func() {
 			It("should return the provided audiences", func() {
 				specAudiences := []string{"https://example.com"}
-				Expect(cluster.GetEffectiveClusterAudiences(fakeClient, logger, &specAudiences, cluster.GetSAToken)).To(Equal(specAudiences))
+				Expect(cluster.GetEffectiveClusterAudiences(nil, logger, &specAudiences, cluster.GetSAToken)).To(Equal(specAudiences))
 			})
 		})
 
 		When("non-default audiences are fetched successfully", func() {
 			It("should return the fetched audiences", func() {
-				mockAudiences := []string{"https://mock.audience.com"}
-				mockGetSAToken := func() (string, error) {
-					return "mock_token", nil
+				stubAudiences := []string{"https://stub.audience.com"}
+				stubGetSAToken := func() (string, error) {
+					return "stub_token", nil
 				}
 
-				baseClient := fake.NewClientBuilder().WithScheme(testScheme).Build()
-				fakeClient := &mockAudiencesClient{Client: baseClient, mockAudiences: mockAudiences}
+				fakeClient := &stubAudiencesClient{stubAudiences: stubAudiences}
 
-				Expect(cluster.GetEffectiveClusterAudiences(fakeClient, logger, nil, mockGetSAToken)).To(Equal(mockAudiences))
+				Expect(cluster.GetEffectiveClusterAudiences(fakeClient, logger, nil, stubGetSAToken)).To(Equal(stubAudiences))
 			})
 		})
 
 		When("an error occurs while fetching the audiences", func() {
 			It("should return the default audiences", func() {
-				mockFailGettingToken := func() (string, error) {
+				stubFailGettingToken := func() (string, error) {
 					return "", fmt.Errorf("we failed getting token")
 				}
 
-				Expect(cluster.GetEffectiveClusterAudiences(fakeClient, logger, nil, mockFailGettingToken)).To(Equal(defaultAudiences))
+				Expect(cluster.GetEffectiveClusterAudiences(nil, logger, nil, stubFailGettingToken)).To(Equal(defaultAudiences))
 			})
 		})
 	})
 })
 
-type mockAudiencesClient struct {
+type stubAudiencesClient struct {
 	client.Client
-	mockAudiences []string
+	stubAudiences []string
 }
 
-func (mac *mockAudiencesClient) Create(ctx context.Context, obj client.Object, opts ...client.CreateOption) error {
+// we stub this rather than mock the client as controller runtime fake client does not support Reactors.
+func (mac *stubAudiencesClient) Create(_ context.Context, obj client.Object, _ ...client.CreateOption) error {
 	if tokenReview, isTokenReview := obj.(*authentication.TokenReview); isTokenReview {
-		tokenReview.Status.Audiences = mac.mockAudiences
+		tokenReview.Status.Audiences = mac.stubAudiences
 		tokenReview.Status.Authenticated = true
 		return nil
 	}
-	return mac.Client.Create(ctx, obj, opts...)
+	return nil
 }
