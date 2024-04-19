08b74a7ce1dd0e005151893159d0f3a8846c3c87
Bartosz Majsak <bartosz.majsak@gmail.com>
2024-04-19 08:23:49 +0200

    chore: removes dead code and adds godoc (#973)
    * chore: removes dead code and adds godoc
    
    * review: wording in the godoc
    
    Co-authored-by: Wen Zhou <wenzhou@redhat.com>
    
    ---------
    
    Co-authored-by: Wen Zhou <wenzhou@redhat.com>

--- pkg/cluster/resources.go
+++ pkg/cluster/resources.go
@@ -79,6 +79,9 @@ func CreateSecret(cli client.Client, name, namespace string, metaOptions ...Meta
 	return nil
 }
 
+// CreateOrUpdateConfigMap creates a new configmap or updates an existing one.
+// If the configmap already exists, it will be updated with the merged Data and MetaOptions, if any.
+// ConfigMap.ObjectMeta.Name and ConfigMap.ObjectMeta.Namespace are both required, it returns an error otherwise.
 func CreateOrUpdateConfigMap(c client.Client, desiredCfgMap *corev1.ConfigMap, metaOptions ...MetaOptions) error {
 	if desiredCfgMap.GetName() == "" || desiredCfgMap.GetNamespace() == "" {
 		return fmt.Errorf("configmap name and namespace must be set")
