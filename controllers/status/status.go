57397f3a72dd6d5c146dc44bad4ce0a383718d4b
Wen Zhou <wenzhou@redhat.com>
2024-04-23 11:18:50 +0200

    Revert "chore: removes unused statuses (#937)"
    This reverts commit d882d3b654165ed9642f68f1f76ca30fcfbc0dcc.

--- controllers/status/status.go
+++ controllers/status/status.go
@@ -63,6 +63,8 @@ const (
 	ReconcileCompleted                    = "ReconcileCompleted"
 	ReconcileCompletedWithComponentErrors = "ReconcileCompletedWithComponentErrors"
 	ReconcileCompletedMessage             = "Reconcile completed successfully"
+	DuplicateDataScienceCluster           = "DuplicateDataScienceCluster"
+	DuplicateDSCInitialization            = "DuplicateDSCInitialization"
 
 	// ConditionReconcileComplete represents extra Condition Type, used by .Condition.Type.
 	ConditionReconcileComplete conditionsv1.ConditionType = "ReconcileComplete"
