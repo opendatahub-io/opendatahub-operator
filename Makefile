opendatahub-io/opendatahub-operator

commit 3610b0ba0cf19bb32853d9d513c2f42ff8ab9f2d

feat: remove leader election for operator (#1000)
- since our design is for only one instance of pod, there is no need to enable
leader-election
- in a shady env. this can cause error

Signed-off-by: Wen Zhou <wenzhou@redhat.com>

bundle/manifests/opendatahub-operator.clusterserviceversion.yaml
@@ -1674,7 +1674,6 @@ spec:
               - args:
                 - --health-probe-bind-address=:8081
                 - --metrics-bind-address=0.0.0.0:8080
-                - --leader-elect
                 - --operator-name=opendatahub
                 command:
                 - /manager

bundle/manifests/opendatahub-operator-manager-config_v1_configmap.yaml
@@ -9,19 +9,6 @@ data:
       bindAddress: 0.0.0.0:8080
     webhook:
       port: 9443
-    leaderElection:
-      leaderElect: true
-      resourceName: 07ed84f7.opendatahub.io
-    # leaderElectionReleaseOnCancel defines if the leader should step down volume
-    # when the Manager ends. This requires the binary to immediately end when the
-    # Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
-    # speeds up voluntary leader transitions as the new leader don't have to wait
-    # LeaseDuration time first.
-    # In the default scaffold provided, the program ends immediately after
-    # the manager stops, so would be fine to enable this option. However,
-    # if you are doing or is intended to do any operation such as perform cleanups
-    # after the manager stops then its usage might be unsafe.
-    # leaderElectionReleaseOnCancel: true
 kind: ConfigMap
 metadata:
   name: opendatahub-operator-manager-config

config/default/manager_auth_proxy_patch.yaml
@@ -13,4 +13,3 @@ spec:
         args:
         - "--health-probe-bind-address=:8081"
         - "--metrics-bind-address=0.0.0.0:8080"
-        - "--leader-elect"

config/manager/controller_manager_config.yaml
@@ -6,16 +6,3 @@ metrics:
   bindAddress: 0.0.0.0:8080
 webhook:
   port: 9443
-leaderElection:
-  leaderElect: true
-  resourceName: 07ed84f7.opendatahub.io
-# leaderElectionReleaseOnCancel defines if the leader should step down volume
-# when the Manager ends. This requires the binary to immediately end when the
-# Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
-# speeds up voluntary leader transitions as the new leader don't have to wait
-# LeaseDuration time first.
-# In the default scaffold provided, the program ends immediately after
-# the manager stops, so would be fine to enable this option. However,
-# if you are doing or is intended to do any operation such as perform cleanups
-# after the manager stops then its usage might be unsafe.
-# leaderElectionReleaseOnCancel: true

config/manager/manager.yaml
@@ -41,7 +41,6 @@ spec:
           - name: DISABLE_DSC_CONFIG
             value: 'true'
         args:
-        - --leader-elect
         - --operator-name=opendatahub
         image: controller:latest
         imagePullPolicy: Always

main.go
@@ -95,7 +95,6 @@ func init() { //nolint:gochecknoinits
 
 func main() { //nolint:funlen
 	var metricsAddr string
-	var enableLeaderElection bool
 	var probeAddr string
 	var dscApplicationsNamespace string
 	var dscMonitoringNamespace string
@@ -104,9 +103,6 @@ func main() { //nolint:funlen
 
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
 	flag.StringVar(&dscApplicationsNamespace, "dsc-applications-namespace", "opendatahub", "The namespace where data science cluster"+
 		"applications will be deployed")
 	flag.StringVar(&dscMonitoringNamespace, "dsc-monitoring-namespace", "opendatahub", "The namespace where data science cluster"+
@@ -118,24 +114,11 @@ func main() { //nolint:funlen
 
 	ctrl.SetLogger(logger.ConfigLoggers(logmode))
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{ // single pod does not need to have LeaderElection
 		Scheme:                 scheme,
 		MetricsBindAddress:     metricsAddr,
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "07ed84f7.opendatahub.io",
-		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
-		// when the Manager ends. This requires the binary to immediately end when the
-		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
-		// speeds up voluntary leader transitions as the new leader don't have to wait
-		// LeaseDuration time first.
-		//
-		// In the default scaffold provided, the program ends immediately after
-		// the manager stops, so would be fine to enable this option. However,
-		// if you are doing or is intended to do any operation such as perform cleanups
-		// after the manager stops then its usage might be unsafe.
-		// LeaderElectionReleaseOnCancel: true,
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")

commit c4ffebb16007ebfce783901616feb4d23183fc60

Sync odh-2.12 (#1011)
* Update version to v2.12.0 (#1007)

* upgrade: retry if default DSCI creation fails (#1008)

After removing leader election, operator fails to start if it is
instructed to create default DSCI. Looks like webhook is not ready
by the time:

```
create default DSCI CR.
{"level":"error","ts":"2024-05-13T09:25:58Z","logger":"setup","msg":"unable to create initial setup for the operator","error":"Internal error occurred: failed calling webhook \"operator.opendatahub.io\": failed to call webhook: Post \"https://opendatahub-operator-controller-manager-service.oo-2ts9m.svc:443/validate-opendatahub-io-v1?timeout=10s\": no endpoints available for service \"opendatahub-operator-controller-manager-service\"","stacktrace":"main.main.func1\n\t/workspace/main.go:200\nsigs.k8s.io/controller-runtime/pkg/manager.RunnableFunc.Start\n\t/remote-source/operator/deps/gomod/pkg/mod/sigs.k8s.io/controller-runtime@v0.14.6/pkg/manager/manager.go:336\nsigs.k8s.io/controller-runtime/pkg/manager.(*runnableGroup).reconcile.func1\n\t/remote-source/operator/deps/gomod/pkg/mod/sigs.k8s.io/controller-runtime@v0.14.6/pkg/manager/runnable_group.go:219"}
```
Leader election added some delay.

The problem does not happen in default configuration since it
explicitly disables DSCI creation in the manifests:

```
       containers:
       - command:
         - /manager
         env:
           - name: DISABLE_DSC_CONFIG
             value: 'true'
         args:
         - --operator-name=opendatahub
         image: controller:latest
```

Make a wrapper function cluster.CreateWithRetry for client.Object
creation with timeout. Use hardcoded 5s interval, just seems
reasonable, and timeout in minutes as the parameter.

It requires disable linter nilerr since for the polling function
error in creation is a valid condition, something the function wait
to disappear.

Fixes: 3610b0ba0cf1 ("feat: remove leader election for operator (#1000)")

Makefile
@@ -3,7 +3,7 @@
 # To re-generate a bundle for another specific version without changing the standard setup, you can:
 # - use the VERSION as arg of the bundle target (e.g make bundle VERSION=0.0.2)
 # - use environment variables to overwrite this value (e.g export VERSION=0.0.2)
-VERSION ?= 2.10.1
+VERSION ?= 2.12.0
 # IMAGE_TAG_BASE defines the opendatahub.io namespace and part of the image name for remote images.
 # This variable is used to construct full image tags for bundle and catalog images.
 #

bundle/manifests/opendatahub-operator.clusterserviceversion.yaml
@@ -101,14 +101,14 @@ metadata:
     capabilities: Full Lifecycle
     categories: AI/Machine Learning, Big Data
     certified: "False"
-    containerImage: quay.io/opendatahub/opendatahub-operator:v2.10.0
+    containerImage: quay.io/opendatahub/opendatahub-operator:v2.12.0
     createdAt: "2024-4-22T00:00:00Z"
-    olm.skipRange: '>=1.0.0 <2.11.0'
+    olm.skipRange: '>=1.0.0 <2.12.0'
     operators.operatorframework.io/builder: operator-sdk-v1.24.1
     operators.operatorframework.io/internal-objects: '[dscinitialization.opendatahub.io]'
     operators.operatorframework.io/project_layout: go.kubebuilder.io/v3
     repository: https://github.com/opendatahub-io/opendatahub-operator
-  name: opendatahub-operator.v2.10.1
+  name: opendatahub-operator.v2.12.0
   namespace: placeholder
 spec:
   apiservicedefinitions: {}
@@ -1763,7 +1763,7 @@ spec:
   selector:
     matchLabels:
       component: opendatahub-operator
-  version: 2.10.1
+  version: 2.12.0
   webhookdefinitions:
   - admissionReviewVersions:
     - v1

config/manifests/bases/opendatahub-operator.clusterserviceversion.yaml
@@ -6,12 +6,12 @@ metadata:
     capabilities: Full Lifecycle
     categories: AI/Machine Learning, Big Data
     certified: "False"
-    containerImage: quay.io/opendatahub/opendatahub-operator:v2.10.0
+    containerImage: quay.io/opendatahub/opendatahub-operator:v2.12.0
     createdAt: "2024-4-22T00:00:00Z"
-    olm.skipRange: '>=1.0.0 <2.11.0'
+    olm.skipRange: '>=1.0.0 <2.12.0'
     operators.operatorframework.io/internal-objects: '[dscinitialization.opendatahub.io]'
     repository: https://github.com/opendatahub-io/opendatahub-operator
-  name: opendatahub-operator.v2.11.0
+  name: opendatahub-operator.v2.12.0
   namespace: placeholder
 spec:
   apiservicedefinitions: {}
@@ -105,4 +105,4 @@ spec:
   selector:
     matchLabels:
       component: opendatahub-operator
-  version: 2.11.0
+  version: 2.12.0

pkg/cluster/resources.go
@@ -174,3 +174,16 @@ func WaitForDeploymentAvailable(ctx context.Context, c client.Client, componentN
 		return true, nil
 	})
 }
+
+func CreateWithRetry(ctx context.Context, cli client.Client, obj client.Object, timeoutMin int) error {
+	interval := time.Second * 5 // arbitrary value
+	timeout := time.Duration(timeoutMin) * time.Minute
+
+	return wait.PollUntilContextTimeout(ctx, interval, timeout, true, func(ctx context.Context) (bool, error) {
+		err := cli.Create(ctx, obj)
+		if err != nil {
+			return false, nil //nolint:nilerr
+		}
+		return true, nil
+	})
+}

pkg/upgrade/upgrade.go
@@ -166,7 +166,7 @@ func CreateDefaultDSCI(ctx context.Context, cli client.Client, _ cluster.Platfor
 		return nil
 	case len(instances.Items) == 0:
 		fmt.Println("create default DSCI CR.")
-		err := cli.Create(ctx, defaultDsci)
+		err := cluster.CreateWithRetry(ctx, cli, defaultDsci, 1) // 1 min timeout
 		if err != nil {
 			return err
 		}