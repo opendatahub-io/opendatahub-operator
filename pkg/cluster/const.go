4fed6891fa6a53a1e6f93a71e1328c2546f7ff26
Vaishnavi Hire <vhire@redhat.com>
2024-05-21 22:17:31 -0400

    Fix CSV name for RHOAI
    (cherry picked from commit 0ed8f48db0ec27b29391366b6566651922cce2e0)

--- pkg/cluster/const.go
+++ pkg/cluster/const.go
@@ -4,7 +4,7 @@ const (
 	// ManagedRhods defines expected addon catalogsource.
 	ManagedRhods Platform = "addon-managed-odh-catalog"
 	// SelfManagedRhods defines display name in csv.
-	SelfManagedRhods Platform = "Red Hat OpenShift Data Science"
+	SelfManagedRhods Platform = "Red Hat OpenShift AI"
 	// OpenDataHub defines display name in csv.
 	OpenDataHub Platform = "Open Data Hub Operator"
 	// Unknown indicates that operator is not deployed using OLM.
