30ea5eac939dd85b51daaa0d55e3e32356cbf776
AJAY JAGANATHAN <ajaganat@redhat.com>
2024-06-07 16:51:38 +0530

    restore webhook

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ import (
 	datascienceclustercontrollers "github.com/opendatahub-io/opendatahub-operator/v2/controllers/datasciencecluster"
 	dscicontr "github.com/opendatahub-io/opendatahub-operator/v2/controllers/dscinitialization"
 	"github.com/opendatahub-io/opendatahub-operator/v2/controllers/secretgenerator"
+	"github.com/opendatahub-io/opendatahub-operator/v2/controllers/webhook"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/cluster"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/logger"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/upgrade"
@@ -127,7 +128,7 @@ func main() { //nolint:funlen
 		os.Exit(1)
 	}
 
-	// (&webhook.OpenDataHubWebhook{}).SetupWithManager(mgr)
+	(&webhook.OpenDataHubWebhook{}).SetupWithManager(mgr)
 
 	if err = (&dscicontr.DSCInitializationReconciler{
 		Client:                mgr.GetClient(),
