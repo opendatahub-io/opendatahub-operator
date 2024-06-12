261bbabab1f8f2f2ae0aa5e628e679e1b55c141d
Yauheni Kaliuta <ykaliuta@redhat.com>
2024-06-12 19:25:19 +0300

    cluster: GetPlatform: replace CSV list with OperatorExists calls (#1051)
    * tests: envtest: Add OperatorCondition CRD
    
    Add OLM's[1] OperatorCondition/OperatorConditionList to the external
    CRDs. Will be used in future patches.
    
    [1] https://github.com/operator-framework/operator-lifecycle-manager/blob/master/deploy/upstream/manifests/0.18.3/0000_50_olm_00-operatorconditions.crd.yaml
    
    Signed-off-by: Yauheni Kaliuta <ykaliuta@redhat.com>
    
    * tests: dscinitialization: add OperatorCondition CRD to schema
    
    Following patches will change initialization to list the objects.
    
    Signed-off-by: Yauheni Kaliuta <ykaliuta@redhat.com>
    
    * cluster: GetPlatform: replace CSV list with OperatorExists calls
    
    Jira: https://issues.redhat.com/browse/RHOAIENG-8483
    
    Depending of the way operators installed CSVs can be seen in all
    namespace so listing of them causes producing N * M results (where N
    is number of namespaces and M is number of CSVs) which is not
    scalable in general and in practice causes timeouts on such large
    clusters.
    
    The function basically checks if ODH or RHOAI operator installed and
    there is already such function in the package, OperatorExists(). So,
    reuse it.
    
    Signed-off-by: Yauheni Kaliuta <ykaliuta@redhat.com>
    
    ---------
    
    Signed-off-by: Yauheni Kaliuta <ykaliuta@redhat.com>

--- controllers/dscinitialization/suite_test.go
+++ controllers/dscinitialization/suite_test.go
@@ -25,6 +25,7 @@ import (
 	routev1 "github.com/openshift/api/route/v1"
 	userv1 "github.com/openshift/api/user/v1"
 	ofapi "github.com/operator-framework/api/pkg/operators/v1alpha1"
+	ofapiv2 "github.com/operator-framework/api/pkg/operators/v2"
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -108,6 +109,7 @@ var _ = BeforeSuite(func() {
 	utilruntime.Must(apiextv1.AddToScheme(testScheme))
 	utilruntime.Must(appsv1.AddToScheme(testScheme))
 	utilruntime.Must(ofapi.AddToScheme(testScheme))
+	utilruntime.Must(ofapiv2.AddToScheme(testScheme))
 	utilruntime.Must(routev1.Install(testScheme))
 	utilruntime.Must(userv1.Install(testScheme))
 	utilruntime.Must(kfdefv1.AddToScheme(testScheme))
