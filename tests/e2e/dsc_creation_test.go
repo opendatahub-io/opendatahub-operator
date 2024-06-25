4578d74e00f827886879a49795b026c4711f08df
Wen Zhou <wenzhou@redhat.com>
2024-06-25 15:16:37 +0200

    fix: make ingress cert secrect with type "kubernetes.io/tls" (#1077)
    - update selfsigned cert if previous one has a different type
    - create new one with kubernetes.io/tls as type
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    (cherry picked from commit ea827dd1e439fafc0670685bb1ef4218a77a4d75)

--- tests/e2e/dsc_creation_test.go
+++ tests/e2e/dsc_creation_test.go
@@ -17,6 +17,7 @@ import (
 	operatorv1 "github.com/openshift/api/operator/v1"
 	"github.com/stretchr/testify/require"
 	autoscalingv1 "k8s.io/api/autoscaling/v1"
+	corev1 "k8s.io/api/core/v1"
 	k8serr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
