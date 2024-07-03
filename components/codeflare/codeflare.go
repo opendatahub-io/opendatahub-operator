36e8fb46489ae2e4f8e402fcdb7a1955a60fd98c
ajaypratap003 <ajay.pratap233@gmail.com>
2024-07-03 21:06:57 +0530

    fix cyclop linters issues

--- components/codeflare/codeflare.go
+++ components/codeflare/codeflare.go
@@ -59,40 +59,16 @@ func (c *CodeFlare) GetComponentName() string {
 
 func (c *CodeFlare) ReconcileComponent(ctx context.Context, cli client.Client, logger logr.Logger, owner metav1.Object, dscispec *dsciv1.DSCInitializationSpec, _ bool) error {
 	l := c.ConfigComponentLogger(logger, ComponentName, dscispec)
-	var imageParamMap = map[string]string{
-		"codeflare-operator-controller-image": "RELATED_IMAGE_ODH_CODEFLARE_OPERATOR_IMAGE", // no need mcad, embedded in cfo
-		"namespace":                           dscispec.ApplicationsNamespace,
-	}
-
 	enabled := c.GetManagementState() == operatorv1.Managed
-	monitoringEnabled := dscispec.Monitoring.ManagementState == operatorv1.Managed
 	platform, err := cluster.GetPlatform(cli)
+
 	if err != nil {
 		return err
 	}
+
 	if enabled {
-		if c.DevFlags != nil {
-			// Download manifests and update paths
-			if err = c.OverrideManifests(string(platform)); err != nil {
-				return err
-			}
-		}
-		// check if the CodeFlare operator is installed: it should not be installed
-		// Both ODH and RHOAI should have the same operator name
-		dependentOperator := CodeflareOperator
-
-		if found, err := cluster.OperatorExists(cli, dependentOperator); err != nil {
-			return fmt.Errorf("operator exists throws error %w", err)
-		} else if found {
-			return fmt.Errorf("operator %s is found. Please uninstall the operator before enabling %s component",
-				dependentOperator, ComponentName)
-		}
-
-		// Update image parameters only when we do not have customized manifests set
-		if (dscispec.DevFlags == nil || dscispec.DevFlags.ManifestsUri == "") && (c.DevFlags == nil || len(c.DevFlags.Manifests) == 0) {
-			if err := deploy.ApplyParams(ParamsPath, imageParamMap, true); err != nil {
-				return fmt.Errorf("failed update image from %s : %w", CodeflarePath+"/bases", err)
-			}
+		if err := c.handleManagedState(cli, platform, dscispec); err != nil {
+			return err
 		}
 	}
 
@@ -104,6 +80,44 @@ func (c *CodeFlare) ReconcileComponent(ctx context.Context, cli client.Client, l
 		return err
 	}
 	l.Info("apply manifests done")
+
+	return c.deployMonitoringConfig(ctx, cli, l, owner, dscispec, platform, enabled)
+}
+
+func (c *CodeFlare) handleManagedState(cli client.Client, platform cluster.Platform, dscispec *dsciv1.DSCInitializationSpec) error {
+	var imageParamMap = map[string]string{
+		"codeflare-operator-controller-image": "RELATED_IMAGE_ODH_CODEFLARE_OPERATOR_IMAGE", // no need mcad, embedded in cfo
+		"namespace":                           dscispec.ApplicationsNamespace,
+	}
+
+	if c.DevFlags != nil {
+		// Download manifests and update paths
+		if err := c.OverrideManifests(string(platform)); err != nil {
+			return err
+		}
+	}
+	// check if the CodeFlare operator is installed: it should not be installed
+	// Both ODH and RHOAI should have the same operator name
+	dependentOperator := CodeflareOperator
+
+	if found, err := cluster.OperatorExists(cli, dependentOperator); err != nil {
+		return fmt.Errorf("operator exists throws error %w", err)
+	} else if found {
+		return fmt.Errorf("operator %s is found. Please uninstall the operator before enabling %s component",
+			dependentOperator, ComponentName)
+	}
+
+	// Update image parameters only when we do not have customized manifests set
+	if (dscispec.DevFlags == nil || dscispec.DevFlags.ManifestsUri == "") && (c.DevFlags == nil || len(c.DevFlags.Manifests) == 0) {
+		if err := deploy.ApplyParams(ParamsPath, imageParamMap, true); err != nil {
+			return fmt.Errorf("failed update image from %s : %w", CodeflarePath+"/bases", err)
+		}
+	}
+	return nil
+}
+
+func (c *CodeFlare) deployMonitoringConfig(ctx context.Context, cli client.Client, logr logr.Logger,
+	owner metav1.Object, dscispec *dsciv1.DSCInitializationSpec, platform cluster.Platform, enabled bool) error {
 	// CloudServiceMonitoring handling
 	if platform == cluster.ManagedRhods {
 		if enabled {
@@ -111,20 +125,21 @@ func (c *CodeFlare) ReconcileComponent(ctx context.Context, cli client.Client, l
 			if err := cluster.WaitForDeploymentAvailable(ctx, cli, ComponentName, dscispec.ApplicationsNamespace, 20, 2); err != nil {
 				return fmt.Errorf("deployment for %s is not ready to server: %w", ComponentName, err)
 			}
-			l.Info("deployment is done, updating monitoring rules")
+			logr.Info("deployment is done, updating monitoring rules")
 		}
 
+		monitoringEnabled := dscispec.Monitoring.ManagementState == operatorv1.Managed
 		// inject prometheus codeflare*.rules in to /opt/manifests/monitoring/prometheus/prometheus-configs.yaml
-		if err = c.UpdatePrometheusConfig(cli, enabled && monitoringEnabled, ComponentName); err != nil {
+		if err := c.UpdatePrometheusConfig(cli, enabled && monitoringEnabled, ComponentName); err != nil {
 			return err
 		}
-		if err = deploy.DeployManifestsFromPath(cli, owner,
+		if err := deploy.DeployManifestsFromPath(cli, owner,
 			filepath.Join(deploy.DefaultManifestPath, "monitoring", "prometheus", "apps"),
 			dscispec.Monitoring.Namespace,
 			"prometheus", true); err != nil {
 			return err
 		}
-		l.Info("updating SRE monitoring done")
+		logr.Info("updating SRE monitoring done")
 	}
 
 	return nil
