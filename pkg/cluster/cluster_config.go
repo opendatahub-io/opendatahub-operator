a8d0ed8eaed04fbcc394718314c22943ff986cf5
bartoszmajsak <bartosz.majsak@gmail.com>
2024-07-03 11:49:25 +0200

    fix: do not reconcile trusted bundle on ns deletion
    When the namespace managed by the operator is deleted the reconcile is
    triggered trying to inject trusted CA bundle to it, even though the
    namespace does not exist at this point.
    
    This is due to the default [`DeleteFunc`](https://github.com/kubernetes-sigs/controller-runtime/blob/e28a8424d69df972355952277dba0e7f0d4263f2/pkg/predicate/predicate.go#L84-L90) being used that triggers
    reconcile.
    
    In this commit the predicate has been changed to ignore all Delete
    events.
    
    In addition redundant `DeleteFunc` always returning true for
    `ConfigMapChangedPredicate` has been removed.

--- pkg/cluster/cluster_config.go
+++ pkg/cluster/cluster_config.go
@@ -5,10 +5,12 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/blang/semver/v4"
 	"github.com/operator-framework/api/pkg/lib/version"
 	ofapi "github.com/operator-framework/api/pkg/operators/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
 	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	k8serr "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -42,6 +44,11 @@ func GetDomain(ctx context.Context, c client.Client) (string, error) {
 func GetOperatorNamespace() (string, error) {
 	data, err := os.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
 	return string(data), err
+}
+
+func IsNotReservedNamespace(ns *corev1.Namespace) bool {
+	return !strings.HasPrefix(ns.GetName(), "openshift-") && !strings.HasPrefix(ns.GetName(), "kube-") &&
+		ns.GetName() != "default" && ns.GetName() != "openshift"
 }
 
 // GetClusterServiceVersion retries the clusterserviceversions available in the operator namespace.
