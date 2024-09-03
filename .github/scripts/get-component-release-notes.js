1e2e7627c08832dcfb6e0baf6dfdaedfcac989aa
AJAY JAGANATHAN <ajaganat@redhat.com>
2024-09-03 22:19:54 +0530

    remove switch

--- .github/scripts/get-component-release-notes.js
+++ .github/scripts/get-component-release-notes.js
@@ -1,37 +1,8 @@
-
-function getComponentName(name){
-    switch(name){
-        case "codeflare":
-            return "Codeflare"
-        case "ray":
-            return "Ray"
-        case "kueue":
-            return "Kueue"
-        case "data-science-pipelines-operator":
-            return "Datascience Pipelines"
-        case "odh-dashboard":
-            return "Dashboard"
-        case "notebook-controller":
-            return "Notebook controller"
-        case "notebooks":
-            return "Notebooks"
-        case "trustyai":
-            return "TrustyAI"
-        case "model-mesh":
-            return "Model mesh"
-        case "odh-model-controller":
-            return "ODH Model Controller"
-        case "kserve":
-            return "Kserve"
-        case "modelregistry":
-            return "Model Registry"
-        case "trainingoperator":
-            return "Training operator"
-        default:
-            return null
-    }   
+function getModifiedComponentName(name){
+    let modifiedWord = name.split("-").join(" ")
+    modifiedWord = modifiedWord[0].toUpperCase() + modifiedWord.slice(1).toLowerCase()
+    return modifiedWord.replace("Odh", "ODH")
 }
-
 module.exports = ({ github, core }) => {
     const { TRACKER_URL } = process.env
     console.log(`The TRACKER_URL is ${TRACKER_URL}`)
@@ -50,9 +21,6 @@ module.exports = ({ github, core }) => {
         }
     }).then((result) => {
         let outputStr = "## Component Release Notes\n"
-        let servingComponents = '- **Serving**\n\t'
-        let distributedWorkloadComponents = '- **Distributed Workloads**\n\t'
-        let workbenchComponents= '- **Workbench**\n\t'
         result.data.forEach((issue) => {
             let issueCommentBody = issue.body_text
             if (issueCommentBody.includes("#Release#")) {
@@ -64,26 +32,13 @@ module.exports = ({ github, core }) => {
                     if (regex.test(component)) {
                         let [componentName, branchUrl, tagUrl] = component.split("|")
                         componentName = componentName.trim()
-                        let releaseNotesUrl = tagUrl
-                        if(!releaseNotesUrl){
-                            releaseNotesUrl = branchUrl
-                        }
-                        releaseNotesUrl = releaseNotesUrl.trim()
-                        if(["model-mesh", "kserve","odh-model-controller"].includes(componentName)){
-                           servingComponents+=` - ${getComponentName(componentName)}: ${releaseNotesUrl}\n\t`
-                        }else if(["codeflare", "ray", "kueue", "trainingoperator"].includes(componentName)){
-                            distributedWorkloadComponents+=` - ${getComponentName(componentName)}: ${releaseNotesUrl}\n\t`
-                        }else if(["notebooks", "notebook-controller"].includes(componentName)){
-                            workbenchComponents+=` - ${getComponentName(componentName)}: ${releaseNotesUrl}\n\t`
-                        }else{
-                            outputStr+= getComponentName(componentName)?`- **${getComponentName(componentName)}**: ${releaseNotesUrl}\n`:""
-                        }
+                        const releaseNotesUrl = (tagUrl || branchUrl).trim();
+                        outputStr += `- **${getModifiedComponentName(componentName)}**: ${releaseNotesUrl}\n`
         
                     }
                 })
             }
         })
-        outputStr+=servingComponents.slice(0,-1)+distributedWorkloadComponents.slice(0,-1)+workbenchComponents.slice(0,-1)
         console.log("Created component release notes successfully...")
         core.setOutput('release-notes-body', outputStr);
     }).catch(e => {
