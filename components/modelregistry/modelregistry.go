4d059a08fc24e60def16ab8b63898c7d060b4d8c
Dhiraj Bokde <dhirajsb@users.noreply.github.com>
2024-09-10 19:53:50 -0700

    fix: add namespace name pattern for registriesNamespace, allow empty string to replace with default value

--- components/modelregistry/modelregistry.go
+++ components/modelregistry/modelregistry.go
@@ -53,6 +53,8 @@ type ModelRegistry struct {
 
 	// Namespace for model registries to be installed, configurable only once when model registry is enabled, defaults to "odh-model-registries"
 	// +kubebuilder:default="odh-model-registries"
+	// +kubebuilder:validation:Pattern="^([a-z0-9]([-a-z0-9]*[a-z0-9])?)?$"
+	// +kubebuilder:validation:MaxLength=63
 	RegistriesNamespace string `json:"registriesNamespace,omitempty"`
 }
 
