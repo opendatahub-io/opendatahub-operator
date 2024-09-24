842e80e8b4ea815205fd941fa79e9c9aedf14c32
AJAY JAGANATHAN <ajaganat@redhat.com>
2024-09-24 21:21:42 +0530

    (fix): ODH release automation: Release notes generation  - The GH action softprops/action-gh-release@v2 currently doesnot support choosing a tag range for the change-log. ie we cannot set a previous tag.    ref: https://github.com/softprops/action-gh-release/issues/465    This fix is aimed to work around it by using the GH api directly and creating the body of the release notes ourselves.  - A minor fix which prevents gh action from doing a merge-commit.

--- .github/scripts/get-component-release-notes.js
+++ .github/scripts/get-component-release-notes.js
@@ -1,27 +1,51 @@
-function getModifiedComponentName(name){
+function getModifiedComponentName(name) {
     let modifiedWord = name.split("-").join(" ").replace(/[^a-zA-Z ]/g, "").trim()
     modifiedWord = modifiedWord[0].toUpperCase() + modifiedWord.slice(1).toLowerCase()
     return modifiedWord.replace("Odh", "ODH")
 }
-module.exports = ({ github, core }) => {
-    const { TRACKER_URL } = process.env
-    console.log(`The TRACKER_URL is ${TRACKER_URL}`)
-    const arr = TRACKER_URL.split("/")
+module.exports = async ({ github, core, context }) => {
+    const { TRACKER_URL: trackerUrl, VERSION: currentTag } = process.env
+    console.log(`The TRACKER_URL is ${trackerUrl}`)
+    const arr = trackerUrl.split("/")
     const owner = arr[3]
     const repo = arr[4]
     const issue_number = arr[6]
 
-    github.request('GET /repos/{owner}/{repo}/issues/{issue_number}/comments', {
-        owner,
-        repo,
-        issue_number,
-        headers: {
-            'X-GitHub-Api-Version': '2022-11-28',
-            'Accept': 'application/vnd.github.text+json'
-        }
-    }).then((result) => {
+    try {
+        const latestReleaseResult = await github.rest.repos.getLatestRelease({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28',
+                'Accept': 'application/vnd.github+json',
+            }
+        })
+        const previousTag = latestReleaseResult.data["tag_name"]
+        console.log(`The current tag is: ${previousTag}`)
+
+        const releaseNotesResult = await github.rest.repos.generateReleaseNotes({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            tag_name: currentTag,
+            previous_tag_name: previousTag,
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28',
+                'Accept': 'application/vnd.github+json'
+            }
+        })
+        const releaseNotesString = releaseNotesResult.data["body"]
+
+        const commentsResult = await github.rest.issues.listComments({
+            owner,
+            repo,
+            issue_number,
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28',
+                'Accept': 'application/vnd.github.text+json'
+            }
+        })
         let outputStr = "## Component Release Notes\n"
-        result.data.forEach((issue) => {
+        commentsResult.data.forEach((issue) => {
             let issueCommentBody = issue.body_text
             if (issueCommentBody.includes("#Release#")) {
                 let components = issueCommentBody.split("\n")
@@ -33,15 +57,16 @@ module.exports = ({ github, core }) => {
                         let [componentName, branchUrl, tagUrl] = component.split("|")
                         componentName = getModifiedComponentName(componentName.trim())
                         const releaseNotesUrl = (tagUrl || branchUrl).trim();
-                        if(!outputStr.includes(componentName)) outputStr += `- **${componentName}**: ${releaseNotesUrl}\n`
-        
+                        if (!outputStr.includes(componentName)) outputStr += `- **${componentName}**: ${releaseNotesUrl}\n`
+
                     }
                 })
             }
         })
+        outputStr += "\n" + releaseNotesString
         console.log("Created component release notes successfully...")
         core.setOutput('release-notes-body', outputStr);
-    }).catch(e => {
-        core.setFailed(`Action failed with error ${e}`);
-    })
+    } catch (error) {
+        core.setFailed(`Action failed with error ${error}`);
+    }
 }
