b4eda47d085b09120a59e7333325131fc19934f9
Wen Zhou <wenzhou@redhat.com>
2024-10-28 09:24:44 +0100

    update: remove two SA which does not seem valid (#1254)
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    (cherry picked from commit c0e07092ecf591fd46ec2b94f93226aade9166c4)

--- components/modelmeshserving/modelmeshserving.go
+++ components/modelmeshserving/modelmeshserving.go
@@ -107,9 +107,7 @@ func (m *ModelMeshServing) ReconcileComponent(ctx context.Context,
 
 		if err := cluster.UpdatePodSecurityRolebinding(ctx, cli, dscispec.ApplicationsNamespace,
 			"modelmesh",
-			"modelmesh-controller",
-			"odh-prometheus-operator",
-			"prometheus-custom"); err != nil {
+			"modelmesh-controller"); err != nil {
 			return err
 		}
 		// Update image parameters
