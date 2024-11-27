72969887e14bb9aac77c4f7db0060324e3003443
Hannah DeFazio <h2defazio@gmail.com>
2024-11-27 03:14:58 -0500

    Use incubating for odh-model-controller manifests (#1393)
    * Use incubating for odh-model-controller manifests
    
    * Use kserve v0.14 branch for manifests

--- get_all_manifests.sh
+++ get_all_manifests.sh
@@ -16,8 +16,8 @@ declare -A COMPONENT_MANIFESTS=(
     ["notebooks"]="opendatahub-io:notebooks:main:manifests:notebooks"
     ["trustyai"]="trustyai-explainability:trustyai-service-operator:main:config:trustyai-service-operator"
     ["model-mesh"]="opendatahub-io:modelmesh-serving:release-0.12.0-rc0:config:model-mesh"
-    ["odh-model-controller"]="opendatahub-io:odh-model-controller:release-0.12.0:config:odh-model-controller"
-    ["kserve"]="opendatahub-io:kserve:release-v0.12.1:config:kserve"
+    ["odh-model-controller"]="opendatahub-io:odh-model-controller:incubating:config:odh-model-controller"
+    ["kserve"]="opendatahub-io:kserve:release-v0.14:config:kserve"
     ["modelregistry"]="opendatahub-io:model-registry-operator:main:config:model-registry-operator"
     ["trainingoperator"]="opendatahub-io:training-operator:dev:manifests:trainingoperator"
 )
