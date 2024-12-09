8ba9073628150076bc3d90d2cadb6185463b32c5
Luca Burgazzoli <lburgazzoli@gmail.com>
2024-12-09 20:40:16 +0100

    Fix findings

--- pkg/controller/actions/deploy/action_deploy_support.go
+++ pkg/controller/actions/deploy/action_deploy_support.go
@@ -6,7 +6,20 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/cluster/gvk"
 )
+
+func isLegacyOwnerRef(or metav1.OwnerReference) bool {
+	switch {
+	case or.APIVersion == gvk.DataScienceCluster.GroupVersion().String() && or.Kind == gvk.DataScienceCluster.Kind:
+		return true
+	case or.APIVersion == gvk.DSCInitialization.GroupVersion().String() && or.Kind == gvk.DSCInitialization.Kind:
+		return true
+	default:
+		return false
+	}
+}
 
 // removeOwnerReferences removes all owner references from a Kubernetes object that match the provided predicate.
 //
