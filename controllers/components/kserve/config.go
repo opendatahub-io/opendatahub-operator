326e4fc9ff1e7712cc32d60e5081722c87ea63b7
Gerard Ryan <git@grdryn.xyz>
2025-01-06 02:37:32 +0000

    Show KServe defaultDeploymentMode in DSC status (#1465)
    JIRA: https://issues.redhat.com/browse/RHOAIENG-16240
    
    The dashboard would like to be able to read this value, and should
    only read it from the status.
    
    The value for the status is always read from the KServe ConfigMap,
    rather than ever assuming that what is set in the DSC, or what the
    operator defaults to, is correct: this is because that ConfigMap can
    be annotated to be unmanaged by the operator, in which case a user may
    have changed the value to something else, and the status should
    reflect this reality.

---
+++ controllers/components/kserve/config.go
@@ -0,0 +1,26 @@
+package kserve
+
+import (
+	"encoding/json"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+)
+
+// ConfigMap Keys.
+const (
+	DeployConfigName     = "deploy"
+	IngressConfigKeyName = "ingress"
+)
+
+type DeployConfig struct {
+	DefaultDeploymentMode string `json:"defaultDeploymentMode,omitempty"`
+}
+
+func getDeployConfig(cm *corev1.ConfigMap) (*DeployConfig, error) {
+	deployConfig := DeployConfig{}
+	if err := json.Unmarshal([]byte(cm.Data[DeployConfigName]), &deployConfig); err != nil {
+		return nil, fmt.Errorf("error retrieving value for key '%s' from ConfigMap %s. %w", DeployConfigName, cm.Name, err)
+	}
+	return &deployConfig, nil
+}
