4b5fc1fbbcd04f84ab39526265020a4f95233e46
Luca Burgazzoli <lburgazzoli@gmail.com>
2025-01-20 12:39:18 +0100

    Fix after rebase

--- tests/e2e/helper_test.go
+++ tests/e2e/helper_test.go
@@ -32,6 +32,7 @@ import (
 )
 
 const (
+	knativeServingNamespace  = "knative-serving"
 	servicemeshNamespace     = "openshift-operators"
 	servicemeshOpName        = "servicemeshoperator"
 	serverlessOpName         = "serverless-operator"
