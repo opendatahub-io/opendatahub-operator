628375d69499bc3638abb09229cac40d0d921567
Yauheni Kaliuta <ykaliuta@redhat.com>
2025-01-24 17:29:47 +0200

    actions: use resourcecacher for manifests rendering
    All the run() functionality now implemented by the ResourceCacher,
    so just call its Render() in run().
    
    Amend WithCache() as required by ResourceCacher to set its
    CachingKeyFn and remove CachingKeyFn initialization from NewAction.
    
    Change render() signature to return resources.UnstructuredList
    
    RendererEngine is used as a field initializer now, so no need to
    export, rename to rendererEngine.
    
    Signed-off-by: Yauheni Kaliuta <ykaliuta@redhat.com>

--- pkg/controller/actions/render/kustomize/action_render_manifests.go
+++ pkg/controller/actions/render/kustomize/action_render_manifests.go
@@ -1,34 +1,27 @@
 package kustomize
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"strings"
 
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"sigs.k8s.io/kustomize/kyaml/filesys"
 
-	"github.com/opendatahub-io/opendatahub-operator/v2/apis/common"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions"
-	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/render"
+	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/resourcecacher"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/types"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/manifests/kustomize"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/resources"
 )
 
-const RendererEngine = "kustomize"
+const rendererEngine = "kustomize"
 
 // Action takes a set of manifest locations and render them as Unstructured resources for
 // further processing. The Action can eventually cache the results in memory to avoid doing
 // a full manifest rendering when not needed.
 type Action struct {
+	resourcecacher.ResourceCacher
+
 	keOpts []kustomize.EngineOptsFn
 	ke     *kustomize.Engine
-
-	cachingKeyFn    render.CachingKeyFn
-	cachingKey      []byte
-	cachedResources resources.UnstructuredList
 }
 
 type ActionOpts func(*Action)
@@ -71,58 +64,16 @@ func WithManifestsOptions(values ...kustomize.EngineOptsFn) ActionOpts {
 
 func WithCache() ActionOpts {
 	return func(action *Action) {
-		action.cachingKeyFn = types.Hash
+		action.ResourceCacher.SetKeyFn(types.Hash)
 	}
 }
 
-func (a *Action) run(_ context.Context, rr *types.ReconciliationRequest) error {
-	var err error
-	var cachingKey []byte
-
-	inst, ok := rr.Instance.(common.WithDevFlags)
-	if ok && inst.GetDevFlags() != nil {
-		// if dev flags are enabled, caching is disabled as dev flags are meant for
-		// development time only where caching is not relevant
-		a.cachingKey = nil
-	} else {
-		cachingKey, err = a.cachingKeyFn(rr)
-		if err != nil {
-			return fmt.Errorf("unable to calculate checksum of reconciliation object: %w", err)
-		}
-	}
-
-	var result resources.UnstructuredList
-
-	if len(cachingKey) != 0 && bytes.Equal(cachingKey, a.cachingKey) && len(a.cachedResources) != 0 {
-		result = a.cachedResources
-	} else {
-		res, err := a.render(rr)
-		if err != nil {
-			return fmt.Errorf("unable to render reconciliation object: %w", err)
-		}
-
-		result = res
-
-		if len(cachingKey) != 0 {
-			a.cachingKey = cachingKey
-			a.cachedResources = result
-		}
-
-		controllerName := strings.ToLower(rr.Instance.GetObjectKind().GroupVersionKind().Kind)
-		render.RenderedResourcesTotal.WithLabelValues(controllerName, RendererEngine).Add(float64(len(result)))
-
-		rr.Generated = true
-	}
-
-	// deep copy object so changes done in the pipelines won't
-	// alter them
-	rr.Resources = append(rr.Resources, result.Clone()...)
-
-	return nil
+func (a *Action) run(ctx context.Context, rr *types.ReconciliationRequest) error {
+	return a.ResourceCacher.Render(ctx, rr, a.render)
 }
 
-func (a *Action) render(rr *types.ReconciliationRequest) ([]unstructured.Unstructured, error) {
-	result := make([]unstructured.Unstructured, 0)
+func (a *Action) render(ctx context.Context, rr *types.ReconciliationRequest) (resources.UnstructuredList, error) {
+	result := make(resources.UnstructuredList, 0)
 
 	for i := range rr.Manifests {
 		renderedResources, err := a.ke.Render(
@@ -142,9 +93,7 @@ func (a *Action) render(rr *types.ReconciliationRequest) ([]unstructured.Unstruc
 
 func NewAction(opts ...ActionOpts) actions.Fn {
 	action := Action{
-		cachingKeyFn: func(rr *types.ReconciliationRequest) ([]byte, error) {
-			return nil, nil
-		},
+		ResourceCacher: resourcecacher.NewResourceCacher(rendererEngine),
 	}
 
 	for _, opt := range opts {
