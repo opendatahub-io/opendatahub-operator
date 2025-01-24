739ab8d8d9d853595eac321c1176dc798ca6e8a1
Luca Burgazzoli <lburgazzoli@gmail.com>
2025-01-24 17:23:00 +0100

    It takes more than 10 minutes for kserve-controller to be created during 2.17 installation

--- pkg/feature/servicemesh/conditions.go
+++ pkg/feature/servicemesh/conditions.go
@@ -90,6 +90,9 @@ func WaitForControlPlaneToBeReady(ctx context.Context, cli client.Client, f *fea
 
 	return wait.PollUntilContextTimeout(ctx, interval, duration, false, func(ctx context.Context) (bool, error) {
 		ready, err := CheckControlPlaneComponentReadiness(ctx, cli, smcp, smcpNs)
+		if k8serr.IsNotFound(err) {
+			return false, nil
+		}
 
 		if ready {
 			f.Log.Info("done waiting for control plane components to be ready", "control-plane", smcp, "namespace", smcpNs)
@@ -112,8 +115,11 @@ func CheckControlPlaneComponentReadiness(ctx context.Context, c client.Client, s
 	}
 
 	components, found, err := unstructured.NestedMap(smcpObj.Object, "status", "readiness", "components")
-	if err != nil || !found {
+	if err != nil {
 		return false, fmt.Errorf("status conditions not found or error in parsing of Service Mesh Control Plane: %w", err)
+	}
+	if !found {
+		return false, nil
 	}
 
 	readyComponents := len(components["ready"].([]interface{}))     //nolint:forcetypeassert,errcheck
