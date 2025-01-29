426c0480f28946d7cac3633a98c99ef10825dd33
Wen Zhou <wenzhou@redhat.com>
2025-01-29 11:02:58 +0100

    fix: missing permission to update prometheusrules
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    (cherry picked from commit fd4c11ba57f6dde8de7827855d311a2f594d89f5)
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- controllers/datasciencecluster/kubebuilder_rbac.go
+++ controllers/datasciencecluster/kubebuilder_rbac.go
@@ -203,7 +203,7 @@ package datasciencecluster
 // +kubebuilder:rbac:groups=components.platform.opendatahub.io,resources=kueues,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=components.platform.opendatahub.io,resources=kueues/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=components.platform.opendatahub.io,resources=kueues/finalizers,verbs=update
-// +kubebuilder:rbac:groups="monitoring.coreos.com",resources=prometheusrules,verbs=get;create;patch;delete;deletecollection;list;watch
+// +kubebuilder:rbac:groups="monitoring.coreos.com",resources=prometheusrules,verbs=get;create;patch;delete;deletecollection;list;watch;update
 // +kubebuilder:rbac:groups="monitoring.coreos.com",resources=podmonitors,verbs=get;create;delete;update;watch;list;patch
 // +kubebuilder:rbac:groups="admissionregistration.k8s.io",resources=validatingadmissionpolicybindings,verbs=get;create;delete;update;watch;list;patch
 // +kubebuilder:rbac:groups="admissionregistration.k8s.io",resources=validatingadmissionpolicies,verbs=get;create;delete;update;watch;list;patch
