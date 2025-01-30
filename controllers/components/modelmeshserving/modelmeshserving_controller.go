c4a2e3dd821706fdac0f475f1f5a59f7b7e188c4
Ugo Giordano <ugiordan@redhat.com>
2025-01-30 12:07:31 +0100

    [sync] fix: Prevent containers from running as root by removing unnecessary anyuid SCC bindings (#1590)
    * Removed Dashboard ServiceAccount from default RoleBinding granting anyuid SCC.
    
    * Removed ModelMesh ServiceAccounts from default RoleBinding granting anyuid SCC.
    
    * Removed ModelController ServiceAccount from default RoleBinding granting anyuid SCC.
    
    * Removed Notebook ServiceAccount from default RoleBinding granting anyuid SCC.
    
    * Remove NewUpdatePodSecurityRoleBindingAction, its corresponding test, and the security folder containing both, as they are no longer needed.
    
    * Remove default rolebinding and its corresponding tests.
    
    * Clean up default RoleBinding during Operator upgrade
    
    * Clean up RoleBinding from function doc
    
    Clean up RoleBinding from function doc

--- controllers/components/modelmeshserving/modelmeshserving_controller.go
+++ controllers/components/modelmeshserving/modelmeshserving_controller.go
@@ -34,7 +34,6 @@ import (
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/deploy"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/gc"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/render/kustomize"
-	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/security"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/updatestatus"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/handlers"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/predicates/clusterrole"
@@ -86,7 +85,6 @@ func (s *componentHandler) NewComponentReconciler(ctx context.Context, mgr ctrl.
 		// Add ModelMeshServing specific actions
 		WithAction(initialize).
 		WithAction(devFlags).
-		WithAction(security.NewUpdatePodSecurityRoleBindingAction(serviceAccounts)).
 		WithAction(kustomize.NewAction(
 			kustomize.WithCache(),
 			kustomize.WithLabel(labels.ODH.Component(LegacyComponentName), labels.True),
