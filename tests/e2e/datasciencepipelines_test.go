c32dc9ecec672e901fec3bd674d9bf8585a44970
Luca Burgazzoli <lburgazzoli@gmail.com>
2025-01-31 10:44:59 +0100

    e2e tests

--- tests/e2e/datasciencepipelines_test.go
+++ tests/e2e/datasciencepipelines_test.go
@@ -3,9 +3,15 @@ package e2e_test
 import (
 	"testing"
 
+	operatorv1 "github.com/openshift/api/operator/v1"
 	"github.com/stretchr/testify/require"
 
 	componentApi "github.com/opendatahub-io/opendatahub-operator/v2/apis/components/v1alpha1"
+	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/cluster/gvk"
+	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/utils/test/matchers/jq"
+	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/utils/test/testf"
+
+	. "github.com/onsi/gomega"
 )
 
 func dataSciencePipelinesTestSuite(t *testing.T) {
@@ -19,6 +25,7 @@ func dataSciencePipelinesTestSuite(t *testing.T) {
 	}
 
 	t.Run("Validate component enabled", componentCtx.ValidateComponentEnabled)
+	t.Run("Validate component managed pipelines", componentCtx.validateManagedPipelines)
 	t.Run("Validate operands have OwnerReferences", componentCtx.ValidateOperandsOwnerReferences)
 	t.Run("Validate update operand resources", componentCtx.ValidateUpdateDeploymentsResources)
 	t.Run("Validate component disabled", componentCtx.ValidateComponentDisabled)
@@ -26,4 +33,51 @@ func dataSciencePipelinesTestSuite(t *testing.T) {
 
 type DataSciencePipelinesTestCtx struct {
 	*ComponentTestCtx
+}
+
+func (c *DataSciencePipelinesTestCtx) validateManagedPipelines(t *testing.T) {
+	t.Run("instructLab", c.validateInstructLabPipelines)
+}
+
+func (c *DataSciencePipelinesTestCtx) validateInstructLabPipelines(t *testing.T) {
+	pipelineType := "instructLab"
+
+	states := []operatorv1.ManagementState{
+		operatorv1.Managed,
+		operatorv1.Removed,
+	}
+
+	for _, state := range states {
+		t.Run(string(state), func(t *testing.T) {
+			g := c.NewWithT(t)
+
+			g.Update(
+				gvk.DataScienceCluster,
+				c.DSCName,
+				testf.TransformPipeline(
+					testf.Transform(
+						`.spec.components.%s.managedPipelines.%s.state = "%s"`,
+						componentApi.DataSciencePipelinesComponentName,
+						pipelineType,
+						state),
+				),
+			).Eventually().Should(
+				jq.Match(
+					`.spec.components.%s.managedPipelines.%s.state == "%s"`,
+					componentApi.DataSciencePipelinesComponentName,
+					pipelineType,
+					state),
+			)
+
+			g.List(gvk.DataSciencePipelines).Eventually().Should(And(
+				HaveLen(1),
+				HaveEach(And(
+					jq.Match(
+						`.spec.managedPipelines.%s.state == "%s"`,
+						pipelineType,
+						state),
+				)),
+			))
+		})
+	}
 }
