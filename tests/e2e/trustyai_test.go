d40a91f70dfbd9cac313c8f278cb8fefc1e10c67
Luca Burgazzoli <lburgazzoli@users.noreply.github.com>
2025-02-04 20:53:51 +0100

    e2e: enable kserve before running TrustyAI tests (#1610)
    * e2e: enable kserve before running TrustyAI tests
    
    The TrustyAI component requires some CRDs shipped as part of the kserve
    component to be present in the cluster befoire starting the controller.
    
    * fix findigs

--- tests/e2e/trustyai_test.go
+++ tests/e2e/trustyai_test.go
@@ -3,9 +3,16 @@ package e2e_test
 import (
 	"testing"
 
+	operatorv1 "github.com/openshift/api/operator/v1"
 	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	componentApi "github.com/opendatahub-io/opendatahub-operator/v2/apis/components/v1alpha1"
+	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/cluster/gvk"
+	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/utils/test/matchers/jq"
+	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/utils/test/testf"
+
+	. "github.com/onsi/gomega"
 )
 
 func trustyAITestSuite(t *testing.T) {
@@ -18,13 +25,53 @@ func trustyAITestSuite(t *testing.T) {
 		ComponentTestCtx: ct,
 	}
 
+	// TrustyAI requires some CRDs that are shipped by Kserve
+	t.Run("Enable Kserve", componentCtx.enableKserve)
+
 	t.Run("Validate component enabled", componentCtx.ValidateComponentEnabled)
 	t.Run("Validate operands have OwnerReferences", componentCtx.ValidateOperandsOwnerReferences)
 	t.Run("Validate update operand resources", componentCtx.ValidateUpdateDeploymentsResources)
 	t.Run("Validate component disabled", componentCtx.ValidateComponentDisabled)
 	t.Run("Validate component releases", componentCtx.ValidateComponentReleases)
+
+	t.Run("Disable Kserve", componentCtx.disableKserve)
 }
 
 type TrustyAITestCtx struct {
 	*ComponentTestCtx
+}
+
+func (c *TrustyAITestCtx) enableKserve(t *testing.T) {
+	g := c.NewWithT(t)
+
+	g.Update(
+		gvk.DataScienceCluster,
+		c.DSCName,
+		testf.Transform(`.spec.components.%s.managementState = "%s"`, componentApi.KserveComponentName, operatorv1.Managed),
+	).Eventually().Should(
+		jq.Match(`.spec.components.%s.managementState == "%s"`, componentApi.KserveComponentName, operatorv1.Managed),
+	)
+
+	g.List(gvk.Kserve).Eventually().Should(And(
+		HaveLen(1),
+		HaveEach(And(
+			jq.Match(`.status.conditions[] | select(.type == "Ready") | .status == "%s"`, metav1.ConditionTrue),
+		)),
+	))
+}
+
+func (c *TrustyAITestCtx) disableKserve(t *testing.T) {
+	g := c.NewWithT(t)
+
+	g.Update(
+		gvk.DataScienceCluster,
+		c.DSCName,
+		testf.Transform(`.spec.components.%s.managementState = "%s"`, componentApi.KserveComponentName, operatorv1.Removed),
+	).Eventually().Should(
+		jq.Match(`.spec.components.%s.managementState == "%s"`, componentApi.KserveComponentName, operatorv1.Removed),
+	)
+
+	g.List(gvk.Kserve).Eventually().Should(
+		BeEmpty(),
+	)
 }
