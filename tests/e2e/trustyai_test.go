dc64ee1bf1859f28fa718de4f481678c101c5a6d
Wen Zhou <wenzhou@redhat.com>
2025-02-11 17:40:05 +0100

    feat: add gate in TrustyAI if ISVC CRD is not in the cluster (#1620)
    * update: add gate in TrustyAI if ISVC CRD is not in the cluster
    
    ---------
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- tests/e2e/trustyai_test.go
+++ tests/e2e/trustyai_test.go
@@ -1,11 +1,17 @@
 package e2e_test
 
 import (
+	"context"
+	"strings"
 	"testing"
 
 	operatorv1 "github.com/openshift/api/operator/v1"
 	"github.com/stretchr/testify/require"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	k8serr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	componentApi "github.com/opendatahub-io/opendatahub-operator/v2/apis/components/v1alpha1"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/cluster/gvk"
@@ -31,7 +37,9 @@ func trustyAITestSuite(t *testing.T) {
 	t.Run("Validate component enabled", componentCtx.ValidateComponentEnabled)
 	t.Run("Validate operands have OwnerReferences", componentCtx.ValidateOperandsOwnerReferences)
 	t.Run("Validate update operand resources", componentCtx.ValidateUpdateDeploymentsResources)
+	t.Run("Validate CRDs reinstated", componentCtx.validateCRDReinstated)
 	t.Run("Validate component disabled", componentCtx.ValidateComponentDisabled)
+	t.Run("Validate pre check", componentCtx.validateTrustyAIPreCheck)
 	t.Run("Validate component releases", componentCtx.ValidateComponentReleases)
 
 	t.Run("Disable Kserve", componentCtx.disableKserve)
@@ -74,4 +82,66 @@ func (c *TrustyAITestCtx) disableKserve(t *testing.T) {
 	g.List(gvk.Kserve).Eventually().Should(
 		BeEmpty(),
 	)
+}
+
+func (c *TrustyAITestCtx) validateCRDReinstated(t *testing.T) {
+	crds := []string{
+		"inferenceservices.serving.kserve.io", // SR is not needed any more in 2.18.0 by TrustyAI
+	}
+
+	for _, crd := range crds {
+		t.Run(crd, func(t *testing.T) {
+			c.ValidateCRDReinstated(t, crd)
+		})
+	}
+}
+
+func (c *TrustyAITestCtx) validateTrustyAIPreCheck(t *testing.T) {
+	// validate precheck on CRD version:
+	// pre-req: skip trusty to removed (done by ValidateComponentDisabled)
+	// step: delete isvc left from enabled Kserve, wait till it is gone
+	// set trustyai to managed, result to error, enable mm, result to success
+
+	g := c.NewWithT(t)
+	g.Delete(gvk.CustomResourceDefinition,
+		types.NamespacedName{Name: "inferenceservices.serving.kserve.io"},
+		client.PropagationPolicy(metav1.DeletePropagationForeground),
+	).Eventually().Should(
+		Succeed(),
+	)
+	g.Eventually(func() bool {
+		var crd apiextensionsv1.CustomResourceDefinition
+		err := c.Client().Get(context.Background(), types.NamespacedName{Name: "inferenceservices.serving.kserve.io"}, &crd)
+		return k8serr.IsNotFound(err)
+	}).Should(BeTrue())
+
+	g.Update(
+		gvk.DataScienceCluster,
+		c.DSCName,
+		testf.Transform(`.spec.components.%s.managementState = "%s"`, strings.ToLower(c.GVK.Kind), operatorv1.Managed),
+	).Eventually().Should(
+		jq.Match(`.spec.components.%s.managementState == "%s"`, strings.ToLower(c.GVK.Kind), operatorv1.Managed),
+	)
+
+	g.List(gvk.DataScienceCluster).Eventually().Should(And(
+		HaveLen(1),
+		HaveEach(
+			jq.Match(`.status.conditions[] | select(.type == "%sReady") | .status == "%s"`, c.GVK.Kind, metav1.ConditionFalse),
+		),
+	))
+
+	g.Update(
+		gvk.DataScienceCluster,
+		c.DSCName,
+		testf.Transform(`.spec.components.%s.managementState = "%s"`, strings.ToLower(componentApi.ModelMeshServingComponentName), operatorv1.Managed),
+	).Eventually().Should(
+		jq.Match(`.spec.components.%s.managementState == "%s"`, strings.ToLower(componentApi.ModelMeshServingComponentName), operatorv1.Managed),
+	)
+
+	g.List(gvk.DataScienceCluster).Eventually().Should(And(
+		HaveLen(1),
+		HaveEach(
+			jq.Match(`.status.conditions[] | select(.type == "%sReady") | .status == "%s"`, c.GVK.Kind, metav1.ConditionTrue),
+		),
+	))
 }
