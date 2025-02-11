b1139c9a73659dfc566ba33437740743199c23e1
Wen Zhou <wenzhou@redhat.com>
2025-02-11 17:40:05 +0100

    feat: add gate in TrustyAI if ISVC CRD is not in the cluster (#1620)
    * update: add gate in TrustyAI if ISVC CRD is not in the cluster
    
    ---------
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    (cherry picked from commit dc64ee1bf1859f28fa718de4f481678c101c5a6d)

--- controllers/components/trustyai/trustyai_controller.go
+++ controllers/components/trustyai/trustyai_controller.go
@@ -24,6 +24,8 @@ import (
 	rbacv1 "k8s.io/api/rbac/v1"
 	extv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
 	componentApi "github.com/opendatahub-io/opendatahub-operator/v2/apis/components/v1alpha1"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/deploy"
@@ -52,10 +54,18 @@ func (s *componentHandler) NewComponentReconciler(ctx context.Context, mgr ctrl.
 			&extv1.CustomResourceDefinition{},
 			reconciler.WithEventHandler(
 				handlers.ToNamed(componentApi.TrustyAIInstanceName)),
-			reconciler.WithPredicates(
-				component.ForLabel(labels.ODH.Component(LegacyComponentName), labels.True)),
+			reconciler.WithPredicates(predicate.Or(
+				component.ForLabel(labels.ODH.Component(LegacyComponentName), labels.True), // if TrustyAI CR is changed
+				predicate.Funcs{ // OR if ISVC from kserve or model-mesh is created
+					CreateFunc: func(e event.CreateEvent) bool {
+						return e.Object.GetName() == "inferenceservices.serving.kserve.io" &&
+							(e.Object.GetLabels()[labels.ODH.Component(componentApi.KserveComponentName)] == labels.True) || (e.Object.GetLabels()[labels.ODH.Component("model-mesh")] == labels.True)
+					},
+				},
+			)),
 		).
 		// Add TrustyAI-specific actions
+		WithAction(checkPreConditions). // check if CRD isvc is there
 		WithAction(initialize).
 		WithAction(devFlags).
 		WithAction(kustomize.NewAction(
