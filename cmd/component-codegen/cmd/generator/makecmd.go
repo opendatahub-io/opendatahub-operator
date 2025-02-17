2b1be046171834ab02a7f322400ffea2c76b4740
AJAY JAGANATHAN <ajaganat@redhat.com>
2025-02-17 19:13:14 +0530

    add build target to post make commands

--- cmd/component-codegen/cmd/generator/makecmd.go
+++ cmd/component-codegen/cmd/generator/makecmd.go
@@ -10,7 +10,7 @@ import (
 // runMakeCommand executes necessary Makefile commands for updating generated code and manifests.
 func RunMakeCommand(logger *logrus.Logger) error {
 	logger.Info("Running make command...")
-	cmd := exec.Command("make", "generate", "manifests", "api-docs", "bundle")
+	cmd := exec.Command("make", "generate", "manifests", "api-docs", "bundle", "build")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
