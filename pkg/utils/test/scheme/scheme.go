4f6ac5cb3289051a29b782a0b7827d27f4a0f77d
Luca Burgazzoli <lburgazzoli@users.noreply.github.com>
2025-02-25 10:23:48 +0100

    Improve test framework (#1689)
    * Improve test framework
    
    - helpers to setup envtest
    - helpers to register common test schemes
    - mock struct for ComponentHandler
    
    * Findings

---
+++ pkg/utils/test/scheme/scheme.go
@@ -0,0 +1,43 @@
+package scheme
+
+import (
+	operatorv1 "github.com/openshift/api/operator/v1"
+	routev1 "github.com/openshift/api/route/v1"
+	ofapi "github.com/operator-framework/api/pkg/operators/v1alpha1"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+
+	componentApi "github.com/opendatahub-io/opendatahub-operator/v2/apis/components/v1alpha1"
+	dscv1 "github.com/opendatahub-io/opendatahub-operator/v2/apis/datasciencecluster/v1"
+	dsciv1 "github.com/opendatahub-io/opendatahub-operator/v2/apis/dscinitialization/v1"
+	featurev1 "github.com/opendatahub-io/opendatahub-operator/v2/apis/features/v1"
+)
+
+var (
+	DefaultAddToSchemes = []func(*runtime.Scheme) error{
+		clientgoscheme.AddToScheme,
+		routev1.AddToScheme,
+		dsciv1.AddToScheme,
+		dscv1.AddToScheme,
+		featurev1.AddToScheme,
+		monitoringv1.AddToScheme,
+		ofapi.AddToScheme,
+		operatorv1.AddToScheme,
+		componentApi.AddToScheme,
+		apiextensionsv1.AddToScheme,
+	}
+)
+
+func New() (*runtime.Scheme, error) {
+	s := runtime.NewScheme()
+
+	for _, at := range DefaultAddToSchemes {
+		if err := at(s); err != nil {
+			return nil, err
+		}
+	}
+
+	return s, nil
+}
