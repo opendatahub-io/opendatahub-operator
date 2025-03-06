82e6309966fe687039cdbf4511e54d214afe0bbc
Luca Burgazzoli <lburgazzoli@users.noreply.github.com>
2025-03-06 07:17:53 +0100

    chore(deps): remove dependency on github.com/pkg/errors and swith to go recommended error wrapping functions (#1715)
    (cherry picked from commit c72410a3f4fab73dc655d2b087319313003cf0fe)

--- pkg/cluster/cert.go
+++ pkg/cluster/cert.go
@@ -15,7 +15,6 @@ import (
 	"time"
 
 	operatorv1 "github.com/openshift/api/operator/v1"
-	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	k8serr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -61,7 +60,7 @@ func CreateSelfSignedCertificate(ctx context.Context, c client.Client, secretNam
 func GenerateSelfSignedCertificateAsSecret(name, addr, namespace string) (*corev1.Secret, error) {
 	cert, key, err := generateCertificate(addr)
 	if err != nil {
-		return nil, errors.WithStack(err)
+		return nil, fmt.Errorf("error generating certificate: %w", err)
 	}
 
 	return &corev1.Secret{
@@ -84,12 +83,12 @@ func GenerateSelfSignedCertificateAsSecret(name, addr, namespace string) (*corev
 func generateCertificate(addr string) ([]byte, []byte, error) {
 	key, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
-		return nil, nil, errors.WithStack(err)
+		return nil, nil, fmt.Errorf("error generating key: %w", err)
 	}
 
 	seededRand, cryptErr := rand.Int(rand.Reader, big.NewInt(time.Now().UnixNano()))
 	if cryptErr != nil {
-		return nil, nil, errors.WithStack(cryptErr)
+		return nil, nil, fmt.Errorf("error generating random: %w", cryptErr)
 	}
 
 	now := time.Now()
@@ -120,11 +119,11 @@ func generateCertificate(addr string) ([]byte, []byte, error) {
 
 	certDERBytes, err := x509.CreateCertificate(rand.Reader, &tmpl, &tmpl, key.Public(), key)
 	if err != nil {
-		return nil, nil, errors.WithStack(err)
+		return nil, nil, fmt.Errorf("error creating certificate: %w", err)
 	}
 	certificate, err := x509.ParseCertificate(certDERBytes)
 	if err != nil {
-		return nil, nil, errors.WithStack(err)
+		return nil, nil, fmt.Errorf("error parsing certificate: %w", err)
 	}
 
 	certBuffer := bytes.Buffer{}
@@ -132,7 +131,7 @@ func generateCertificate(addr string) ([]byte, []byte, error) {
 		Type:  "CERTIFICATE",
 		Bytes: certificate.Raw,
 	}); err != nil {
-		return nil, nil, errors.WithStack(err)
+		return nil, nil, fmt.Errorf("error encoding pem: %w", err)
 	}
 
 	keyBuffer := bytes.Buffer{}
@@ -140,7 +139,7 @@ func generateCertificate(addr string) ([]byte, []byte, error) {
 		Type:  "RSA PRIVATE KEY",
 		Bytes: x509.MarshalPKCS1PrivateKey(key),
 	}); err != nil {
-		return nil, nil, errors.WithStack(err)
+		return nil, nil, fmt.Errorf("error encoding pem: %w", err)
 	}
 
 	return certBuffer.Bytes(), keyBuffer.Bytes(), nil
