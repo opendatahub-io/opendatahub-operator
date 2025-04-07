6b830698d88e1cb91bbf140a0ee28e3d3ee20bcd
Wen Zhou <wenzhou@redhat.com>
2025-04-07 17:29:36 +0200

    update: add support for overlays in Feast
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- internal/controller/components/feastoperator/feastoperator.go
+++ internal/controller/components/feastoperator/feastoperator.go
@@ -56,9 +56,9 @@ func (s *componentHandler) NewCRObject(dsc *dscv1.DataScienceCluster) common.Pla
 	}
 }
 
-func (s *componentHandler) Init(_ common.Platform) error {
-	if err := odhdeploy.ApplyParams(manifestPath().String(), imageParamMap); err != nil {
-		return fmt.Errorf("failed to update images on path %s: %w", manifestPath(), err)
+func (s *componentHandler) Init(p common.Platform) error {
+	if err := odhdeploy.ApplyParams(manifestPath(p).String(), imageParamMap); err != nil {
+		return fmt.Errorf("failed to update images on path %s: %w", manifestPath(p), err)
 	}
 
 	return nil
