1e7ace9a84c0741a3ec9fcddbc6e1f98d78ec7ad
James Tanner <jtanner+github@redhat.com>
2025-05-05 08:20:44 -0400

    wrong file

--- pkg/cluster/cluster_config_test.go
+++ pkg/cluster/cluster_config_test.go
@@ -7,6 +7,7 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors" // Import k8serrors
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
