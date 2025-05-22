12cabefef1fe4bbeb836776201495cf7da39bc72
den-rgb <den02_m@yahoo.com>
2025-05-22 16:01:23 +0100

    RHOAIENG-25367 | feat: Added scraping label to odh resources

--- internal/controller/components/kserve/kserve_controller.go
+++ internal/controller/components/kserve/kserve_controller.go
@@ -36,6 +36,7 @@ import (
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/cluster/gvk"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/deploy"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/gc"
+	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/observability"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/render/kustomize"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/render/template"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/status/deployments"
@@ -151,6 +152,7 @@ func (s *componentHandler) NewComponentReconciler(ctx context.Context, mgr ctrl.
 			kustomize.WithLabel(labels.ODH.Component(LegacyComponentName), labels.True),
 			kustomize.WithLabel(labels.K8SCommon.PartOf, LegacyComponentName),
 		)).
+		WithAction(observability.NewAction()).
 		WithAction(customizeKserveConfigMap).
 		WithAction(deploy.NewAction(
 			deploy.WithCache(),
