e20e92df0c38fd622ea21fc44e7c057be4b55d37
den-rgb <den02_m@yahoo.com>
2025-07-18 10:37:34 +0100

    Sync | RHOAIENG-25354

--- api/services/v1alpha1/monitoring_types.go
+++ api/services/v1alpha1/monitoring_types.go
@@ -136,7 +136,8 @@ type MonitoringCommonSpec struct {
 	Namespace string `json:"namespace,omitempty"`
 	// metrics collection
 	Metrics *Metrics `json:"metrics,omitempty"`
-	Traces  *Traces  `json:"traces,omitempty"`
+	// traces collection
+	Traces *Traces `json:"traces,omitempty"`
 }
 
 //+kubebuilder:object:root=true
