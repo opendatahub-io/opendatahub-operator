f27ce687990df671b4da3eb4ee970f1aba1ca6ec
Maor Friedman <maor.friedman@redhat.com>
2025-07-30 08:29:18 +0300

    Kserve Image Overrides (#2032)
    * prepare kserve for operator-nudging
    
    * sort
    
    * use correct env variables
    
    * add llm-d entries
    
    * fix lint issue
    
    ---------
    
    Co-authored-by: Rishab Prasad <riprasad@redhat.com>
    Co-authored-by: Filippe Spolti <fspolti@redhat.com>

--- internal/controller/components/kserve/kserve_support.go
+++ internal/controller/components/kserve/kserve_support.go
@@ -35,7 +35,13 @@ const DefaultCertificateSecretName = "knative-serving-cert"
 
 var (
 	imageParamMap = map[string]string{
-		"oauth-proxy": "RELATED_IMAGE_OSE_OAUTH_PROXY_IMAGE",
+		"kserve-agent":                     "RELATED_IMAGE_ODH_KSERVE_AGENT_IMAGE",
+		"kserve-controller":                "RELATED_IMAGE_ODH_KSERVE_CONTROLLER_IMAGE",
+		"kserve-router":                    "RELATED_IMAGE_ODH_KSERVE_ROUTER_IMAGE",
+		"kserve-storage-initializer":       "RELATED_IMAGE_ODH_KSERVE_STORAGE_INITIALIZER_IMAGE",
+		"kserve-llm-d-inference-scheduler": "RELATED_IMAGE_ODH_LLM_D_INFERENCE_SCHEDULER_IMAGE",
+		"kserve-llm-d-routing-sidecar":     "RELATED_IMAGE_ODH_LLM_D_ROUTING_SIDECAR_IMAGE",
+		"oauth-proxy":                      "RELATED_IMAGE_OSE_OAUTH_PROXY_IMAGE",
 	}
 )
 
