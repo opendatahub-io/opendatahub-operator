cd63a736c6556523f32d00dc8a07ef39a8b8fcc4
Wen Zhou <wenzhou@redhat.com>
2025-08-01 20:23:59 +0200

    [sync] feat: add webhook for connection on isvc + notebook create/update (#2224+2234) (#2235)
    * feat: add webhook for connection on isvc create/update (#2224)
    
    - create new mwc connection-isvc
    	on resource isvc.serving.kserve.io
    	on operator: create, update
    	have one webhook for both validator and injector
    - validate if request has permitted annotation value
    - inject secret name/decoded value into to-be-created isvc resource
    - update: move common function out to pkg and use it for OCI and S3
    - move to use GvKToPartical to get only metadata from secret, eventually just return Name
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    
    ---------
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    (cherry picked from commit 58ba3a9fd1380471ae1e1a76b78f31cfc7e09dcc)
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    
    * feat: add webhook for notebook on create/update (#2234)
    
    * fix: add API reader to notebook webhook to read secrets in non-cached namespaces
    
    (cherry picked from commit 3601af77c41086b3e5f06623410157921860f7d7)
    
    * fix: cherry-pick error
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    
    ---------
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    Co-authored-by: Carl Kyrillos <80267718+carlkyrillos@users.noreply.github.com>

---
+++ internal/webhook/inferenceservice/register.go
@@ -0,0 +1,21 @@
+//go:build !nowebhook
+
+package inferenceservice
+
+import (
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// RegisterWebhooks registers the combined connection webhook that handles both validation and mutation.
+func RegisterWebhooks(mgr ctrl.Manager) error {
+	if err := (&ConnectionWebhook{
+		Client:  mgr.GetAPIReader(),
+		Decoder: admission.NewDecoder(mgr.GetScheme()),
+		Name:    "connection-isvc",
+	}).SetupWithManager(mgr); err != nil {
+		return err
+	}
+
+	return nil
+}
