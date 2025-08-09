f3a06bd221543ddffab94697ecd916853fe25894
OpenShift Cherrypick Robot <openshift-cherrypick-robot@redhat.com>
2025-08-10 01:28:58 +0200

    [sync] RHOAIENG-31536 - OpenTelemetry and Cluster Observability operators are required to be installed despite no observability configured (#2258)
    * chore: Align otel condition with other observability operators
    
    * chore: Align monitoring controller actions workflow
    
    * RHOAIENG-31536 - OpenTelemetry and Cluster Observability operators are required to be installed despite no observability configured
    
    * fix: enable monitoring namespace for other platform if monitoring is enabled (#2250)
    
    * fix: enable moniotoring for other platform if monitoring is enabled
    
    - ODH and self-managed : is monitoring is enabled
    - managed : always
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    
    * fix: wrong logic, the one should always create namespace is Managed
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    
    ---------
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    
    * RHOAIENG-31643 - otel-collector is not deployed when only traces are enabled (#2262)
    
    * RHOAIENG-31497 - Monitoring namespace is not created on self-managed clusters - display error (#2257)
    
    This commit doesn't fix the issue but allows any NotFound error - e.g. when a namespace if a resource to be created is missing, to be displayed to users.
    
    ---------
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    Co-authored-by: Marián Macik <mmacik@redhat.com>
    Co-authored-by: Wen Zhou <wenzhou@redhat.com>
    Co-authored-by: Marián Macik <macik.marian@gmail.com>

--- internal/controller/status/status.go
+++ internal/controller/status/status.go
@@ -103,7 +103,7 @@ const (
 	ConditionMonitoringAvailable             = "MonitoringAvailable"
 	ConditionMonitoringStackAvailable        = "MonitoringStackAvailable"
 	ConditionTempoAvailable                  = "TempoAvailable"
-	ConditionOpenTelemetryCollectorAvailable = "OpenTelemetryCollectorCRDAvailable"
+	ConditionOpenTelemetryCollectorAvailable = "OpenTelemetryCollectorAvailable"
 	ConditionInstrumentationAvailable        = "InstrumentationAvailable"
 )
 
