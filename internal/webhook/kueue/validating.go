2372ff72561ac011f7c9f73128c7abde12d0d9dc
Wen Zhou <wenzhou@redhat.com>
2025-08-12 16:10:24 +0200

    feat: add support for llmisvc for kueue
    - rename old kueue one for isvc so we can have both isvc and llmisvc
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- internal/webhook/kueue/validating.go
+++ internal/webhook/kueue/validating.go
@@ -31,10 +31,12 @@ import (
 // - kubeflow.org/v1: pytorchjobs, notebooks
 // - ray.io/v1 and v1alpha1: rayjobs, rayclusters
 // - serving.kserve.io/v1beta1: inferenceservices
+// - serving.kserve.io/v1alpha1: llminferenceservices
 
 //+kubebuilder:webhook:path=/validate-kueue,mutating=false,failurePolicy=fail,sideEffects=None,groups=kubeflow.org,resources=pytorchjobs;notebooks,verbs=create;update,versions=v1,name=kubeflow-kueuelabels-validator.opendatahub.io,admissionReviewVersions=v1
 //+kubebuilder:webhook:path=/validate-kueue,mutating=false,failurePolicy=fail,sideEffects=None,groups=ray.io,resources=rayjobs;rayclusters,verbs=create;update,versions=v1;v1alpha1,name=ray-kueuelabels-validator.opendatahub.io,admissionReviewVersions=v1
-//+kubebuilder:webhook:path=/validate-kueue,mutating=false,failurePolicy=fail,sideEffects=None,groups=serving.kserve.io,resources=inferenceservices,verbs=create;update,versions=v1beta1,name=kserve-kueuelabels-validator.opendatahub.io,admissionReviewVersions=v1
+//+kubebuilder:webhook:path=/validate-kueue,mutating=false,failurePolicy=fail,sideEffects=None,groups=serving.kserve.io,resources=inferenceservices,verbs=create;update,versions=v1beta1,name=kserve-isvc-kueuelabels-validator.opendatahub.io,admissionReviewVersions=v1
+//+kubebuilder:webhook:path=/validate-kueue,mutating=false,failurePolicy=fail,sideEffects=None,groups=serving.kserve.io,resources=llminferenceservices,verbs=create;update,versions=v1alpha1,name=kserve-llmisvc-kueuelabels-validator.opendatahub.io,admissionReviewVersions=v1
 //nolint:lll
 
 var (
@@ -128,13 +130,14 @@ func (v *Validator) Handle(ctx context.Context, req admission.Request) admission
 func isExpectedKind(kind metav1.GroupVersionKind) bool {
 	// List of expected resource types that the Kueue webhook should handle
 	expectedGVKs := []schema.GroupVersionKind{
-		gvk.Notebook,           // kubeflow.org/v1/Notebook
-		gvk.PyTorchJob,         // kubeflow.org/v1/PyTorchJob
-		gvk.RayJobV1Alpha1,     // ray.io/v1alpha1/RayJob
-		gvk.RayJobV1,           // ray.io/v1/RayJob
-		gvk.RayClusterV1Alpha1, // ray.io/v1alpha1/RayCluster
-		gvk.RayClusterV1,       // ray.io/v1/RayCluster
-		gvk.InferenceServices,  // serving.kserve.io/v1beta1/InferenceService
+		gvk.Notebook,                    // kubeflow.org/v1/Notebook
+		gvk.PyTorchJob,                  // kubeflow.org/v1/PyTorchJob
+		gvk.RayJobV1Alpha1,              // ray.io/v1alpha1/RayJob
+		gvk.RayJobV1,                    // ray.io/v1/RayJob
+		gvk.RayClusterV1Alpha1,          // ray.io/v1alpha1/RayCluster
+		gvk.RayClusterV1,                // ray.io/v1/RayCluster
+		gvk.InferenceServices,           // serving.kserve.io/v1beta1/InferenceService
+		gvk.LLMInferenceServiceV1Alpha1, // serving.kserve.io/v1alpha1/LLMInferenceService
 	}
 
 	requestGVK := schema.GroupVersionKind{
