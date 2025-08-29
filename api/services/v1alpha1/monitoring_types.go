b004427d7372be5a341e92106e60aa1ec0819fd4
Dayakar Maruboena <dmaruboe@redhat.com>
2025-08-29 14:56:41 -0400

    Feat: Enable third party metrics export (#2267)
    Co-authored-by: Dayakar Maruboena <dmaruboe@dmaruboe-thinkpadp1gen7.rht.csb>

--- api/services/v1alpha1/monitoring_types.go
+++ api/services/v1alpha1/monitoring_types.go
@@ -45,7 +45,9 @@ type MonitoringSpec struct {
 type Metrics struct {
 	Storage   *MetricsStorage   `json:"storage,omitempty"`
 	Resources *MetricsResources `json:"resources,omitempty"`
-	// Replicas specifies the number of replicas in monitoringstack, default is 2 if not set
+	// Replicas specifies the number of replicas in monitoringstack. If not set, it defaults
+	// to 1 on single-node clusters and 2 on multi-node clusters.
+	// +kubebuilder:validation:Minimum=0
 	Replicas int32 `json:"replicas,omitempty"`
 	// Exporters defines custom metrics exporters for sending metrics to external observability tools.
 	// Each key-value pair represents an exporter name and its configuration.
