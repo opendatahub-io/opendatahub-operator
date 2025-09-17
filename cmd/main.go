40fff5e871ebb1512754bf7f841d601ed8061e97
Wen Zhou <wenzhou@redhat.com>
2025-09-17 22:56:49 +0200

    update: remove logic set default value in odhdashboardconfig in upgrade (#2469) (#2481)
    (cherry picked from commit 5a27b1403c43a186f3918d66f87cd5b68ed32785)
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -431,20 +431,6 @@ func main() { //nolint:funlen,maintidx,gocyclo
 		}
 	}
 
-	// TODO: to be removed: https://issues.redhat.com/browse/RHOAIENG-21080
-	var patchODCFunc manager.RunnableFunc = func(ctx context.Context) error {
-		if err := upgrade.PatchOdhDashboardConfig(ctx, setupClient, oldReleaseVersion, release); err != nil {
-			setupLog.Error(err, "Unable to patch the odhdashboardconfig")
-			return err
-		}
-		return nil
-	}
-
-	err = mgr.Add(patchODCFunc)
-	if err != nil {
-		setupLog.Error(err, "Error patching odhdashboardconfig")
-	}
-
 	// Cleanup resources from previous v2 releases
 	var cleanExistingResourceFunc manager.RunnableFunc = func(ctx context.Context) error {
 		if err = upgrade.CleanupExistingResource(ctx, setupClient, platform, oldReleaseVersion); err != nil {
