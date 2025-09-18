6ad45f94a6311d82967e9a6419bbd1d540108e9b
resoluteCoder <resoluteCoder@gmail.com>
2025-09-18 16:16:54 -0500

    added better support for oidc, fixed kubeproxy/envoyfilter issue

--- internal/controller/datasciencecluster/kubebuilder_rbac.go
+++ internal/controller/datasciencecluster/kubebuilder_rbac.go
@@ -293,7 +293,7 @@ package datasciencecluster
 // +kubebuilder:rbac:groups=services.platform.opendatahub.io,resources=gatewayconfigs/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=services.platform.opendatahub.io,resources=gatewayconfigs/finalizers,verbs=update
 // Gateway API resources (what the controller actually creates)
-// +kubebuilder:rbac:groups=gateway.networking.k8s.io,resources=gateways;gatewayclasses,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=gateway.networking.k8s.io,resources=gateways;gatewayclasses;httproutes,verbs=get;list;watch;create;update;patch;delete
 
 // FeastOperator
 // +kubebuilder:rbac:groups=components.platform.opendatahub.io,resources=feastoperators,verbs=get;list;watch;create;update;patch;delete
