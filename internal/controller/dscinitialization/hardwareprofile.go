6ae817ee969a0f9b5e96f41561a39d66e736070b
Sukumar Subramani <suksubra@redhat.com>
2025-09-30 19:59:31 +0530

    RHOAIENG-33158 | feat: enhance hardware profile management by adding custom-serving profile
    - Introduced a new HardwareProfile named 'custom-serving' in the YAML configuration.
    - Updated the DSCInitializationReconciler to manage both default and custom hardware profiles.
    - Modified the logic to check for the existence of the custom hardware profile during reconciliation.

--- internal/controller/dscinitialization/hardwareprofile.go
+++ internal/controller/dscinitialization/hardwareprofile.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"path/filepath"
 
+	k8serr "k8s.io/apimachinery/pkg/api/errors"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 
@@ -15,7 +16,7 @@ import (
 )
 
 // deploy hardware profile CR with dsci as owner, but allow user change by annotation set to false.
-func (r *DSCInitializationReconciler) ManageDefaultHWProfileCR(ctx context.Context, dscInit *dsciv2.DSCInitialization, platform common.Platform) error {
+func (r *DSCInitializationReconciler) ManageDefaultAndCustomHWProfileCR(ctx context.Context, dscInit *dsciv2.DSCInitialization, platform common.Platform) error {
 	log := logf.FromContext(ctx)
 
 	if platform == "" { // this is for test to skip creation.
@@ -23,22 +24,32 @@ func (r *DSCInitializationReconciler) ManageDefaultHWProfileCR(ctx context.Conte
 		return nil
 	}
 
-	// Check if default HardwareProfile CR already exists
+	// Check if default HardwareProfile CR already exists.
 	_, err := cluster.GetHardwareProfile(ctx, r.Client, "default-profile", dscInit.Spec.ApplicationsNamespace)
-	if err == nil {
-		log.V(1).Info("HardwareProfile CR 'default-profile' already exists")
-		return nil
-	}
 	if client.IgnoreNotFound(err) != nil {
 		return fmt.Errorf("failed to check default HardwareProfile CR: %w", err)
 	}
 
-	// deploy hardware profile CR with dsci as owner, but allow user change by have annotation in the default.
-	hwProfilePath := filepath.Join(deploy.DefaultManifestPath, "hardwareprofiles")
-	if err := deploy.DeployManifestsFromPath(ctx, r.Client, dscInit, hwProfilePath, dscInit.Spec.ApplicationsNamespace, "hardwareprofile", true); err != nil {
-		return fmt.Errorf("failed to deploy HardwareProfile CR from path %s: %w", hwProfilePath, err)
+	if k8serr.IsNotFound(err) {
+		// deploy hardware profile CR with dsci as owner, but allow user change by have annotation in the default.
+		hwProfilePath := filepath.Join(deploy.DefaultManifestPath, "hardwareprofiles")
+		if err := deploy.DeployManifestsFromPath(ctx, r.Client, dscInit, hwProfilePath, dscInit.Spec.ApplicationsNamespace, "hardwareprofile", true); err != nil {
+			return fmt.Errorf("failed to deploy HardwareProfile CR from path %s: %w", hwProfilePath, err)
+		}
+	}
+	// Check if custom-serving HardwareProfile CR already exists
+	_, err = cluster.GetHardwareProfile(ctx, r.Client, "custom-serving", dscInit.Spec.ApplicationsNamespace)
+	if client.IgnoreNotFound(err) != nil {
+		return fmt.Errorf("failed to check custom-serving HardwareProfile CR: %w", err)
 	}
 
-	log.V(1).Info("Successfully deployed HardwareProfile CR default-profile")
+	if k8serr.IsNotFound(err) {
+		hwProfilePath := filepath.Join(deploy.DefaultManifestPath, "hardwareprofiles")
+		if err := deploy.DeployManifestsFromPath(ctx, r.Client, dscInit, hwProfilePath, dscInit.Spec.ApplicationsNamespace, "hardwareprofile", true); err != nil {
+			return fmt.Errorf("failed to deploy HardwareProfile CR from path %s: %w", hwProfilePath, err)
+		}
+	}
+
+	log.V(1).Info("Successfully deployed default and custom-serving HardwareProfile CR")
 	return nil
 }
