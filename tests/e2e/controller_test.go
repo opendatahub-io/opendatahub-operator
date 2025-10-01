6a7ce99a1349d161ce6fe28c7b018296f8408425
Gowtham Shanmugasundaram <gshanmug@redhat.com>
2025-10-01 17:03:58 +0530

    Disable gatewayTestSuite on rhoai (already disabled on main)
    Signed-off-by: Gowtham Shanmugasundaram <gshanmug@redhat.com>

--- tests/e2e/controller_test.go
+++ tests/e2e/controller_test.go
@@ -136,7 +136,7 @@ var (
 		scenarios: map[string]TestFn{
 			serviceApi.MonitoringServiceName: monitoringTestSuite,
 			serviceApi.AuthServiceName:       authControllerTestSuite,
-			serviceApi.GatewayServiceName:    gatewayTestSuite,
+			// serviceApi.GatewayServiceName:    gatewayTestSuite,
 		},
 	}
 )
