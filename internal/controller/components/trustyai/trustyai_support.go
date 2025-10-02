bfeb60169fae68f01acb725ee9379be272daf95c
Wen Zhou <wenzhou@redhat.com>
2025-10-02 14:04:42 +0200

    refactor: auth
    - rename resources
       GatewayConfig CR: data-science-gatewayconfig
       GatewayClass CR: data-science-gateway-class
       Gateway CR: data-science-gateway
       Envoyfilter CR:  data-science-authn-filter
       DesitnationRules CR: data-science-tls-rule
       HttpRoute for kube-auth-proxy: oauth-callback-route
       HttpRoute for dashboard: default-dashboard-route
       Oauthclient CR: dashobard-oauth-client
       deployment: kube-auth-proxy
       Secret for kube-auth-proxy: kube-auth-proxy-tls
       Secret for GW: default-gateway-tls
       Service for kube-auth-proxy: kube-auth-proxy
    - add owners for resources
       dsci -> gwconfig + DR + all httproute
       GWConfig -> GW + EF + kube-auth-proxy-tls + data-science-gateway-tls
       + kube-auth-proxy svc + kube-auth-proxy deployment
    - move creation to template
    - reuse infra GatewaySpec type
    - simplify logic
    - cleanup condition
    - update DR's .spec.host: one httproute one destinationrule + needed
      port with cert skip
    - create DR for all component svc to use in application namespace
    - explicitly set hostname in httproute: seems something without does not
      route correctly?
    - add secretNamespace for OIDCConfig struct
    - make OAuthClient only be Own if it is integratedAuth since ROSA need
      to be config at Day0 and OAuthClient is not registered
    - create Route for DNS map to GW SVC: this is issue on PSI, need to gap
      the resolved IP to GW
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- internal/controller/components/trustyai/trustyai_support.go
+++ internal/controller/components/trustyai/trustyai_support.go
@@ -29,7 +29,7 @@ var (
 		"guardrails-orchestrator-image":      "RELATED_IMAGE_ODH_FMS_GUARDRAILS_ORCHESTRATOR_IMAGE",
 		"guardrails-sidecar-gateway-image":   "RELATED_IMAGE_ODH_TRUSTYAI_VLLM_ORCHESTRATOR_GATEWAY_IMAGE",
 		"guardrails-built-in-detector-image": "RELATED_IMAGE_ODH_BUILT_IN_DETECTOR_IMAGE",
-		"oauthProxyImage":                    "RELATED_IMAGE_OSE_OAUTH_PROXY_IMAGE",
+		"oauthProxyImage":                    "RELATED_IMAGE_OSE_OAUTH_PROXY_IMAGE", // TODO: remove this.
 		"kube-rbac-proxy":                    "RELATED_IMAGE_OSE_KUBE_RBAC_PROXY_IMAGE",
 	}
 
