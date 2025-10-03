25177e4b845cb16d0b9e42af9d4c5a2545feeb4f
Sukumar Subramani <suksubra@redhat.com>
2025-10-03 07:14:46 +0530

    RHOAIENG-33158, updated dsci from v1 to v2 to align with new version

--- internal/controller/dscinitialization/hardwareprofile.go
+++ internal/controller/dscinitialization/hardwareprofile.go
@@ -46,7 +46,7 @@ func (r *DSCInitializationReconciler) CreateVAP(ctx context.Context, dscInit *ds
 }
 
 // deploy hardware profile CR with dsci as owner, but allow user change by annotation set to false.
-func (r *DSCInitializationReconciler) ManageDefaultAndCustomHWProfileCR(ctx context.Context, dscInit *dsciv1.DSCInitialization, platform common.Platform) error {
+func (r *DSCInitializationReconciler) ManageDefaultAndCustomHWProfileCR(ctx context.Context, dscInit *dsciv2.DSCInitialization, platform common.Platform) error {
 	log := logf.FromContext(ctx)
 
 	if platform == "" { // this is for test to skip creation.
