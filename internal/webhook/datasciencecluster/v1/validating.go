a0ef28ba982186a43393c60132ef321490d90f15
Andrea Tarocchi <andrea.tarocchi@gmail.com>
2025-10-03 17:05:21 +0200

    RHOAIENG-33891: DSC v1 should not be created or updated with Kueue component state set to Managed

--- internal/webhook/datasciencecluster/v1/validating.go
+++ internal/webhook/datasciencecluster/v1/validating.go
@@ -5,7 +5,9 @@ package v1
 import (
 	"context"
 	"fmt"
+	"net/http"
 
+	operatorv1 "github.com/openshift/api/operator/v1"
 	admissionv1 "k8s.io/api/admission/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -13,18 +15,20 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
+	dscv1 "github.com/opendatahub-io/opendatahub-operator/v2/api/datasciencecluster/v1"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/cluster/gvk"
 	webhookutils "github.com/opendatahub-io/opendatahub-operator/v2/pkg/webhook"
 )
 
-//+kubebuilder:webhook:path=/validate-datasciencecluster-v1,mutating=false,failurePolicy=fail,sideEffects=None,groups=datasciencecluster.opendatahub.io,resources=datascienceclusters,verbs=create,versions=v1,name=datasciencecluster-v1-validator.opendatahub.io,admissionReviewVersions=v1
+//+kubebuilder:webhook:path=/validate-datasciencecluster-v1,matchPolicy=Exact,mutating=false,failurePolicy=fail,sideEffects=None,groups=datasciencecluster.opendatahub.io,resources=datascienceclusters,verbs=create;update,versions=v1,name=datasciencecluster-v1-validator.opendatahub.io,admissionReviewVersions=v1
 //nolint:lll
 
 // Validator implements webhook.AdmissionHandler for DataScienceCluster v1 validation webhooks.
 // It enforces singleton creation rules for DataScienceCluster resources and always allows their deletion.
 type Validator struct {
-	Client client.Reader
-	Name   string
+	Client  client.Reader
+	Name    string
+	Decoder admission.Decoder
 }
 
 // Assert that Validator implements admission.Handler interface.
@@ -59,18 +63,50 @@ func (v *Validator) Handle(ctx context.Context, req admission.Request) admission
 	log := logf.FromContext(ctx)
 	ctx = logf.IntoContext(ctx, log)
 
-	var resp admission.Response
+	allowMessage := fmt.Sprintf("Operation %s on %s v1 allowed", req.Operation, req.Kind.Kind)
+
+	if req.Kind.Kind != gvk.DataScienceClusterV1.Kind || req.Kind.Group != gvk.DataScienceClusterV1.Group || req.Kind.Version != gvk.DataScienceClusterV1.Version {
+		err := fmt.Errorf("unexpected gvk: %v; expecting: %v", req.Kind, gvk.DataScienceClusterV1)
+		logf.FromContext(ctx).Error(err, "got wrong group/version/kind")
+		return admission.Errored(http.StatusBadRequest, err)
+	}
 
 	switch req.Operation {
 	case admissionv1.Create:
-		resp = webhookutils.ValidateSingletonCreation(ctx, v.Client, &req, gvk.DataScienceCluster)
+		return validate([]validationCheck{v.denyManagementstateManaged, denyMultipleDsc}, allowMessage, ctx, v.Client, &req)
+	case admissionv1.Update:
+		return validate([]validationCheck{v.denyManagementstateManaged}, allowMessage, ctx, v.Client, &req)
 	default:
-		resp.Allowed = true // initialize Allowed to be true in case Operation falls into "default" case
+		return admission.Allowed(allowMessage) // initialize Allowed to be true in case Operation falls into "default" case
+	}
+}
+
+type validationCheck func(context.Context, client.Reader, *admission.Request) admission.Response
+
+func validate(checks []validationCheck, allowedMessage string, ctx context.Context, client client.Reader, request *admission.Request) admission.Response {
+	for _, check := range checks {
+		resp := check(ctx, client, request)
+		if !resp.Allowed {
+			return resp
+		}
 	}
 
-	if !resp.Allowed {
-		return resp
+	return admission.Allowed(allowedMessage)
+}
+
+func denyMultipleDsc(ctx context.Context, client client.Reader, req *admission.Request) admission.Response {
+	return webhookutils.ValidateSingletonCreation(ctx, client, req, gvk.DataScienceCluster)
+}
+
+func (v *Validator) denyManagementstateManaged(ctx context.Context, client client.Reader, req *admission.Request) admission.Response {
+	dcsV1 := &dscv1.DataScienceCluster{}
+	if err := v.Decoder.DecodeRaw(req.Object, dcsV1); err != nil {
+		logf.FromContext(ctx).Error(err, "Error converting request object to "+gvk.DataScienceClusterV1.String())
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+	if dcsV1.Spec.Components.Kueue.ManagementState == operatorv1.Managed {
+		return admission.Denied("Managed is no longer supported as a managementState")
 	}
 
-	return admission.Allowed(fmt.Sprintf("Operation %s on %s v1 allowed", req.Operation, req.Kind.Kind))
+	return admission.Allowed("")
 }
