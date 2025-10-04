0f2e4957b19755c77f092a7db135d7421bde940e
Luca Burgazzoli <lburgazzoli@users.noreply.github.com>
2025-10-04 16:45:30 +0200

    Add webhook to prevent creation of dashbaord's HardwareProfile and AcceleratorProfile (#2599)

--- internal/controller/dscinitialization/hardwareprofile.go
+++ internal/controller/dscinitialization/hardwareprofile.go
@@ -11,39 +11,8 @@ import (
 	"github.com/opendatahub-io/opendatahub-operator/v2/api/common"
 	dsciv2 "github.com/opendatahub-io/opendatahub-operator/v2/api/dscinitialization/v2"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/cluster"
-	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/cluster/gvk"
-	odherrors "github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/actions/errors"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/deploy"
 )
-
-// CreateVAP creates VAP/VAPB for blocking Dashboard's HWProfile/AcceleratorProfile.
-func (r *DSCInitializationReconciler) CreateVAP(ctx context.Context, dscInit *dsciv2.DSCInitialization) error {
-	log := logf.FromContext(ctx)
-
-	// first check if CRDs in cluster, if no, quick exit
-	apCRDExists, err := cluster.HasCRD(ctx, r.Client, gvk.DashboardAcceleratorProfile)
-	if err != nil {
-		return odherrors.NewStopError("failed to check %s CRDs version: %w", gvk.DashboardAcceleratorProfile, err)
-	}
-	dhpCRDExists, err := cluster.HasCRD(ctx, r.Client, gvk.DashboardHardwareProfile)
-	if err != nil {
-		return odherrors.NewStopError("failed to check %s CRDs version: %w", gvk.DashboardHardwareProfile, err)
-	}
-
-	// proceed if any CRDs exist
-	if !apCRDExists && !dhpCRDExists {
-		log.V(1).Info("Both CRDs not exist, skipping creation for VAP/VAPB")
-		return nil
-	}
-
-	vapPath := filepath.Join(deploy.DefaultManifestPath, "hardwareprofiles", "vap")
-	if err := deploy.DeployManifestsFromPath(ctx, r.Client, dscInit, vapPath, "", "vap", true); err != nil {
-		return fmt.Errorf("failed to deploy VAP/VAPB manifests from path %s: %w", vapPath, err)
-	}
-
-	log.V(1).Info("Successfully deployed VAP/VAPB resources")
-	return nil
-}
 
 // deploy hardware profile CR with dsci as owner, but allow user change by annotation set to false.
 func (r *DSCInitializationReconciler) ManageDefaultHWProfileCR(ctx context.Context, dscInit *dsciv2.DSCInitialization, platform common.Platform) error {
