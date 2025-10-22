dbefc7b6b4f28eced6d7a155dc409a7678ee31e4
Wen Zhou <wenzhou@redhat.com>
2025-10-22 22:08:17 +0200

    feat: use annotation on kube-auth deployment when secret is changed (#2740)
    - calculate secret data to hash and set as annoatation to trigger
      deployment change => pod restart with new secret value
    - cleanu rbac
    - fix lint to use expectedODHDomain
    
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>
    (cherry picked from commit 07804b0b50498b8fe8e1bc43860173ead790e5e3)

--- internal/controller/datasciencecluster/kubebuilder_rbac.go
+++ internal/controller/datasciencecluster/kubebuilder_rbac.go
@@ -274,22 +274,6 @@ package datasciencecluster
 // +kubebuilder:rbac:groups=components.platform.opendatahub.io,resources=modelcontrollers/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=components.platform.opendatahub.io,resources=modelcontrollers/finalizers,verbs=update
 
-// Auth
-// +kubebuilder:rbac:groups=services.platform.opendatahub.io,resources=auths,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=services.platform.opendatahub.io,resources=auths/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=services.platform.opendatahub.io,resources=auths/finalizers,verbs=update
-
-// Gateway
-// CR management
-// +kubebuilder:rbac:groups=services.platform.opendatahub.io,resources=gatewayconfigs,verbs=get;list;watch;create;update;patch
-// +kubebuilder:rbac:groups=services.platform.opendatahub.io,resources=gatewayconfigs/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=services.platform.opendatahub.io,resources=gatewayconfigs/finalizers,verbs=update
-// Gateway API resources (what the controller actually creates)
-// +kubebuilder:rbac:groups=gateway.networking.k8s.io,resources=gateways;gatewayclasses;httproutes,verbs=get;list;watch;create;update;patch;delete
-// Gateway controller creates and manages the following Istio resources
-// +kubebuilder:rbac:groups=networking.istio.io,resources=destinationrules,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=networking.istio.io,resources=envoyfilters,verbs=get;list;watch;create;update;patch;delete
-
 // FeastOperator
 // +kubebuilder:rbac:groups=components.platform.opendatahub.io,resources=feastoperators,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=components.platform.opendatahub.io,resources=feastoperators/status,verbs=get;update;patch
