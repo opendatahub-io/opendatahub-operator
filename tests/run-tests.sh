13c0de48c7120d740def0baa7146e59ec44663a4
cesar <csannico@redhat.com>
2025-11-19 14:27:15 +0100

    test changes

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -5,4 +5,4 @@ set +o allexport
 
 mkdir -p /results
 
-gotestsum .tests/e2e/ --debug --format testname --junitfile results/xunit_report.xml
+gotestsum ./tests/e2e/ --debug --format testname --junitfile results/xunit_report.xml
