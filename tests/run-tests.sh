2e3fca696a1257cad1c156f5054f86cdb0e9de79
cesar <csannico@redhat.com>
2025-11-19 13:51:22 +0100

    test changes

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -5,6 +5,6 @@ set +o allexport
 
 mkdir -p /results
 
-gotestsum ./tests/e2e/ --verbose --junitfile results/xunit_report.xml
+gotestsum ./tests/e2e/ --verbose --format standard-verbose --junitfile results/xunit_report.xml
 
 sleep 120s
