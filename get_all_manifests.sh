eb8fd29eb2ff6798d02b992b482cb828f051e551
Edgar Hernández <ehernand@redhat.com>
2025-12-05 00:42:20 -0600

    feat(maas): add MaaS manifests (#2894)
    This adds Model-as-a-Service manifests references to the get_all_manifests.sh.
    
    Signed-off-by: Edgar Hernández <ehernand@redhat.com>

--- get_all_manifests.sh
+++ get_all_manifests.sh
@@ -28,6 +28,7 @@ declare -A ODH_COMPONENT_MANIFESTS=(
     ["feastoperator"]="opendatahub-io:feast:stable@94bc6204cbdbcbebfd004f64986f43397bfc0e65:infra/feast-operator/config"
     ["llamastackoperator"]="opendatahub-io:llama-stack-k8s-operator:odh@226e911cca9bf7efa1e632860613087b0bf14d74:config"
     ["trainer"]="opendatahub-io:trainer:main@cf3112e978b2d9153750d0c0f422d92143b69464:manifests"
+    ["maas"]="opendatahub-io:maas-billing:main@cf4c743a544482beedd92c9c90366998822c72af:deployment"
 )
 
 # RHOAI Component Manifests
@@ -46,6 +47,7 @@ declare -A RHOAI_COMPONENT_MANIFESTS=(
     ["feastoperator"]="red-hat-data-services:feast:rhoai-3.2:infra/feast-operator/config"
     ["llamastackoperator"]="red-hat-data-services:llama-stack-k8s-operator:rhoai-3.2:config"
     ["trainer"]="red-hat-data-services:trainer:rhoai-3.2:manifests"
+    ["maas"]="red-hat-data-services:maas-billing:rhoai-3.2:deployment"
 )
 
 # Select the appropriate manifest based on platform type
