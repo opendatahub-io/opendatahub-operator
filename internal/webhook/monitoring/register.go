bed711f0493609f753e9ea47b9c0f653b9093276
bro-adm <lielbondy@gmail.com>
2025-12-07 10:24:41 +0200

    [+] odh monitoring label coreos monitors injector mutator webhook
    ### Monitoring - Admission Components - Webhooks - Mutation - Injection
    ###
    
    injects odh monitoring label to coreos monitors on namespaces labeld
    with odh monitoring label

--- internal/webhook/monitoring/register.go
+++ internal/webhook/monitoring/register.go
@@ -3,7 +3,10 @@
 package monitoring
 
 import (
+	"errors"
+
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
 // RegisterWebhooks registers the webhooks for monitoring injection.
@@ -14,6 +17,17 @@ import (
 // Returns:
 //   - error: Any error encountered during webhook registration.
 func RegisterWebhooks(mgr ctrl.Manager) error {
-	// TODO: Implement webhook registration logic
+	if mgr == nil {
+		return errors.New("manager cannot be nil")
+	}
+
+	if err := (&Injector{
+		Client:  mgr.GetAPIReader(),
+		Decoder: admission.NewDecoder(mgr.GetScheme()),
+		Name:    "monitoring-injector",
+	}).SetupWithManager(mgr); err != nil {
+		return err
+	}
+
 	return nil
 }
