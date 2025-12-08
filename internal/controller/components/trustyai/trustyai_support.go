06c8c9da3ef8157be5d2b53fbb8865d26f46940b
Rui Vieira <ruidevieira@googlemail.com>
2025-12-08 10:23:00 +0000

    fix: Use correct garak image name
    Changed to `RELATED_IMAGE_ODH_TRUSTYAI_GARAK_LLS_PROVIDER_DSP_IMAGE`

--- internal/controller/components/trustyai/trustyai_support.go
+++ internal/controller/components/trustyai/trustyai_support.go
@@ -30,7 +30,7 @@ var (
 		"guardrails-sidecar-gateway-image":   "RELATED_IMAGE_ODH_TRUSTYAI_VLLM_ORCHESTRATOR_GATEWAY_IMAGE",
 		"guardrails-built-in-detector-image": "RELATED_IMAGE_ODH_BUILT_IN_DETECTOR_IMAGE",
 		"ragas-provider-image":               "RELATED_IMAGE_ODH_TRUSTYAI_RAGAS_LLS_PROVIDER_DSP_IMAGE",
-		"garak-provider-image":               "RELATED_IMAGE_ODH_TRUSTYAI_GARAK_PROVIDER_IMAGE",
+		"garak-provider-image":               "RELATED_IMAGE_ODH_TRUSTYAI_GARAK_LLS_PROVIDER_DSP_IMAGE",
 		"kube-rbac-proxy":                    "RELATED_IMAGE_OSE_KUBE_RBAC_PROXY_IMAGE",
 	}
 
