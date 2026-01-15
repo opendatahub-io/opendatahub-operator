0a721fafc6e54eb983c75f8b2cc24400b69a2a2d
Gowtham Shanmugasundaram <gshanmug@redhat.com>
2026-01-15 20:35:42 +0530

    fix(trustyai): prevent continuous reconciles from unrelated CRD events (#3075)
    Signed-off-by: Gowtham Shanmugasundaram <gshanmug@redhat.com>

--- internal/controller/datasciencecluster/kubebuilder_rbac.go
+++ internal/controller/datasciencecluster/kubebuilder_rbac.go
@@ -259,6 +259,7 @@ package datasciencecluster
 // OpenVino still need buildconfig
 // +kubebuilder:rbac:groups="build.openshift.io",resources=builds,verbs=create;patch;delete;list;watch;get
 // +kubebuilder:rbac:groups="build.openshift.io",resources=buildconfigs/instantiate,verbs=create;patch;delete;get;list;watch
+// +kubebuilder:rbac:groups="inference.networking.k8s.io",resources=inferencepools,verbs=get;list;watch
 // +kubebuilder:rbac:groups="build.openshift.io",resources=buildconfigs,verbs=list;watch;create;patch;delete;get;update
 
 // DataSciencePipelines
