b3be5d7bd88e426f7fee220d9c045f16798306fe
bro-adm <lielbondy@gmail.com>
2026-01-26 15:03:37 +0200

    [+] admission webhooks registrator unit test
    ### Monitoring - Admission Components - Webhhoks - Unit Tests ###

---
+++ internal/webhook/monitoring/register_test.go
@@ -0,0 +1,45 @@
+//go:build !nowebhook
+
+package monitoring_test
+
+import (
+	"testing"
+
+	"github.com/opendatahub-io/opendatahub-operator/v2/internal/webhook/monitoring"
+	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/utils/test/envt"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestRegisterWebhooks(t *testing.T) {
+	t.Parallel()
+	g := NewWithT(t)
+
+	// Create a test environment with manager
+	env, err := envt.New(
+		envt.WithManager(),
+	)
+	g.Expect(err).ShouldNot(HaveOccurred())
+	defer func() {
+		g.Expect(env.Stop()).To(Succeed())
+	}()
+
+	// Test that RegisterWebhooks succeeds
+	err = monitoring.RegisterWebhooks(env.Manager())
+	g.Expect(err).ShouldNot(HaveOccurred())
+
+	// Verify that the webhook was registered by checking the webhook server
+	webhookServer := env.Manager().GetWebhookServer()
+	g.Expect(webhookServer).ShouldNot(BeNil())
+}
+
+func TestRegisterWebhooks_NilManager(t *testing.T) {
+	t.Parallel()
+	g := NewWithT(t)
+
+	// Test with nil manager - should return error
+	err := monitoring.RegisterWebhooks(nil)
+
+	g.Expect(err).Should(HaveOccurred(), "RegisterWebhooks(nil) should return error")
+	g.Expect(err.Error()).Should(ContainSubstring("manager cannot be nil"))
+}
