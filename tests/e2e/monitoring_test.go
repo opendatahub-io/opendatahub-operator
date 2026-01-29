7150ba4d60d227a3def1db3a76c46311c36387dd
bro-adm <lielbondy@gmail.com>
2026-01-29 18:13:29 +0200

    [+] odh monitoring label coreos monitors injection mutation webhook no patch e2e tests
    ### Monitoring - Admission Components - Webhooks - Mutation - Injection
    - E2E Tests - No Patch Cases ###

--- tests/e2e/monitoring_test.go
+++ tests/e2e/monitoring_test.go
@@ -140,6 +140,11 @@ func monitoringTestSuite(t *testing.T) {
 			TestCase{"Setup monitoring admission components tests", monitoringServiceCtx.ValidateMonitoringWebhookTestsSetup},
 			TestCase{"Validate monitoring label value enforcement on namespace", monitoringServiceCtx.ValidateMonitoringLabelValueEnforcementOnNamespace},
 			TestCase{"Validate monitoring label value enforcement on monitors", monitoringServiceCtx.ValidateMonitoringLabelValueEnforcementOnMonitors},
+			TestCase{"Validate webhook skips non-monitored namespace", monitoringServiceCtx.ValidateWebhookSkipsNonMonitoredNamespace},
+			TestCase{"Validate webhook skips explicitly opted-out namespace", monitoringServiceCtx.ValidateWebhookSkipsExplicitlyOptedOutNamespace},
+			TestCase{"Validate webhook respects user opt-out", monitoringServiceCtx.ValidateWebhookRespectsUserOptOut},
+			TestCase{"Validate webhook idempotency", monitoringServiceCtx.ValidateWebhookIdempotency},
+			TestCase{"Validate webhook does not inject when monitoring disabled", monitoringServiceCtx.ValidateWebhookSkipsWhenMonitoringDisabled},
 		)
 	}
 
