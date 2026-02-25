b1d1e4031cc04d785045982b8e5344a37791f2f9
Edward Arthur Quarm Jnr <equarmjn@redhat.com>
2026-02-25 09:37:36 -0500

    override cpu x86 image in params.env (#3189)

--- internal/controller/components/modelcontroller/modelcontroller_support.go
+++ internal/controller/components/modelcontroller/modelcontroller_support.go
@@ -26,6 +26,7 @@ var (
 		"mlserver-image":          "RELATED_IMAGE_ODH_MLSERVER_IMAGE",
 		"vllm-cuda-image":         "RELATED_IMAGE_RHAIIS_VLLM_CUDA_IMAGE",
 		"vllm-cpu-image":          "RELATED_IMAGE_ODH_VLLM_CPU_IMAGE",
+		"vllm-cpu-x86-image":      "RELATED_IMAGE_RHAIIS_VLLM_CPU_IMAGE",
 		"vllm-gaudi-image":        "RELATED_IMAGE_ODH_VLLM_GAUDI_IMAGE",
 		"vllm-rocm-image":         "RELATED_IMAGE_RHAIIS_VLLM_ROCM_IMAGE",
 		"vllm-spyre-image":        "RELATED_IMAGE_RHAIIS_VLLM_SPYRE_IMAGE",
