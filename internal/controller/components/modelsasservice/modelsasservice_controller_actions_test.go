5991d0c31618b7f4fd06fb0e977fe795b050e4bc
Ishita Sequeira <ishiseq29@gmail.com>
2026-03-06 12:02:29 -0500

    add automatic restart of maas-api pods for any configMap changes

--- internal/controller/components/modelsasservice/modelsasservice_controller_actions_test.go
+++ internal/controller/components/modelsasservice/modelsasservice_controller_actions_test.go
@@ -4,6 +4,8 @@ package modelsasservice
 import (
 	"testing"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -14,6 +16,7 @@ import (
 	componentApi "github.com/opendatahub-io/opendatahub-operator/v2/api/components/v1alpha1"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/cluster/gvk"
 	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/controller/types"
+	"github.com/opendatahub-io/opendatahub-operator/v2/pkg/metadata/labels"
 
 	. "github.com/onsi/gomega"
 )
@@ -584,4 +587,174 @@ func TestAPIKeyConfiguration(t *testing.T) {
 			}
 		})
 	})
+}
+
+func TestConfigureConfigHashAnnotation(t *testing.T) {
+	g := NewWithT(t)
+
+	t.Run("Config Hash Annotation", func(t *testing.T) {
+		t.Run("should add config hash annotation to deployment when ConfigMap exists", func(t *testing.T) {
+			// Create ConfigMap
+			configMap := createConfigMap(MaaSParametersConfigMapName, "opendatahub", map[string]string{
+				"gateway-namespace": "openshift-ingress",
+				"gateway-name":      "maas-default-gateway",
+			})
+
+			// Create Deployment
+			deployment := createDeployment(MaaSAPIDeploymentName, "opendatahub")
+
+			rr := &types.ReconciliationRequest{
+				Resources: []unstructured.Unstructured{configMap, deployment},
+			}
+
+			err := configureConfigHashAnnotation(t.Context(), rr)
+			g.Expect(err).ShouldNot(HaveOccurred())
+
+			// Verify the deployment has the annotation
+			dep := &appsv1.Deployment{}
+			err = runtime.DefaultUnstructuredConverter.FromUnstructured(rr.Resources[1].Object, dep)
+			g.Expect(err).ShouldNot(HaveOccurred())
+
+			annotationKey := labels.ODHAppPrefix + "/MaaSConfigHash"
+			g.Expect(dep.Spec.Template.Annotations).Should(HaveKey(annotationKey))
+			g.Expect(dep.Spec.Template.Annotations[annotationKey]).ShouldNot(BeEmpty())
+		})
+
+		t.Run("should update hash when ConfigMap data changes", func(t *testing.T) {
+			// First ConfigMap
+			configMap1 := createConfigMap(MaaSParametersConfigMapName, "opendatahub", map[string]string{
+				"gateway-namespace":           "openshift-ingress",
+				"gateway-name":                "maas-default-gateway",
+				"api-key-max-expiration-days": "30",
+			})
+			deployment1 := createDeployment(MaaSAPIDeploymentName, "opendatahub")
+
+			rr1 := &types.ReconciliationRequest{
+				Resources: []unstructured.Unstructured{configMap1, deployment1},
+			}
+
+			err := configureConfigHashAnnotation(t.Context(), rr1)
+			g.Expect(err).ShouldNot(HaveOccurred())
+
+			dep1 := &appsv1.Deployment{}
+			err = runtime.DefaultUnstructuredConverter.FromUnstructured(rr1.Resources[1].Object, dep1)
+			g.Expect(err).ShouldNot(HaveOccurred())
+
+			annotationKey := labels.ODHAppPrefix + "/MaaSConfigHash"
+			hash1 := dep1.Spec.Template.Annotations[annotationKey]
+
+			// Second ConfigMap with different data
+			configMap2 := createConfigMap(MaaSParametersConfigMapName, "opendatahub", map[string]string{
+				"gateway-namespace":           "openshift-ingress",
+				"gateway-name":                "maas-default-gateway",
+				"api-key-max-expiration-days": "90", // Changed!
+			})
+			deployment2 := createDeployment(MaaSAPIDeploymentName, "opendatahub")
+
+			rr2 := &types.ReconciliationRequest{
+				Resources: []unstructured.Unstructured{configMap2, deployment2},
+			}
+
+			err = configureConfigHashAnnotation(t.Context(), rr2)
+			g.Expect(err).ShouldNot(HaveOccurred())
+
+			dep2 := &appsv1.Deployment{}
+			err = runtime.DefaultUnstructuredConverter.FromUnstructured(rr2.Resources[1].Object, dep2)
+			g.Expect(err).ShouldNot(HaveOccurred())
+
+			hash2 := dep2.Spec.Template.Annotations[annotationKey]
+
+			// Hashes should be different
+			g.Expect(hash1).ShouldNot(Equal(hash2))
+		})
+
+		t.Run("should succeed silently when ConfigMap is not found", func(t *testing.T) {
+			// Only Deployment, no ConfigMap
+			deployment := createDeployment(MaaSAPIDeploymentName, "opendatahub")
+
+			rr := &types.ReconciliationRequest{
+				Resources: []unstructured.Unstructured{deployment},
+			}
+
+			err := configureConfigHashAnnotation(t.Context(), rr)
+			g.Expect(err).ShouldNot(HaveOccurred())
+		})
+
+		t.Run("should succeed silently when Deployment is not found", func(t *testing.T) {
+			// Only ConfigMap, no Deployment
+			configMap := createConfigMap(MaaSParametersConfigMapName, "opendatahub", map[string]string{
+				"gateway-namespace": "openshift-ingress",
+			})
+
+			rr := &types.ReconciliationRequest{
+				Resources: []unstructured.Unstructured{configMap},
+			}
+
+			err := configureConfigHashAnnotation(t.Context(), rr)
+			g.Expect(err).ShouldNot(HaveOccurred())
+		})
+
+		t.Run("should produce consistent hash for same data", func(t *testing.T) {
+			data := map[string]string{
+				"b-key": "value-b",
+				"a-key": "value-a",
+				"c-key": "value-c",
+			}
+
+			hash1 := hashConfigMapData(data)
+			hash2 := hashConfigMapData(data)
+
+			g.Expect(hash1).Should(Equal(hash2))
+		})
+	})
+}
+
+// createConfigMap creates an unstructured ConfigMap resource for testing.
+func createConfigMap(name, namespace string, data map[string]string) unstructured.Unstructured {
+	cm := &corev1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "ConfigMap",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Data: data,
+	}
+
+	u, _ := runtime.DefaultUnstructuredConverter.ToUnstructured(cm)
+	return unstructured.Unstructured{Object: u}
+}
+
+// createDeployment creates an unstructured Deployment resource for testing.
+func createDeployment(name, namespace string) unstructured.Unstructured {
+	dep := &appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "apps/v1",
+			Kind:       "Deployment",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{},
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "maas-api",
+							Image: "quay.io/opendatahub/maas-api:latest",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	u, _ := runtime.DefaultUnstructuredConverter.ToUnstructured(dep)
+	return unstructured.Unstructured{Object: u}
 }
