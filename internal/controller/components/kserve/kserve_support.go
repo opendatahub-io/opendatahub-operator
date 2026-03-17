0fdea913b656168620a5b934cd7ebe0d8483bb59
Wen Zhou <wenzhou@redhat.com>
2026-03-17 07:13:20 +0100

    update: add support for new llm-d UDS image for kserve (#3285)
    Signed-off-by: Wen Zhou <wenzhou@redhat.com>

--- internal/controller/components/kserve/kserve_support.go
+++ internal/controller/components/kserve/kserve_support.go
@@ -39,6 +39,7 @@ var (
 		"kserve-llm-d-inference-scheduler": "RELATED_IMAGE_ODH_LLM_D_INFERENCE_SCHEDULER_IMAGE",
 		"kserve-llm-d-routing-sidecar":     "RELATED_IMAGE_ODH_LLM_D_ROUTING_SIDECAR_IMAGE",
 		"kube-rbac-proxy":                  "RELATED_IMAGE_OSE_KUBE_RBAC_PROXY_IMAGE",
+		"kserve-llm-d-uds-tokenizer":       "RELATED_IMAGE_ODH_LLM_D_KV_CACHE_IMAGE",
 	}
 )
 
