e952e0f78f7c6f0350ef6f64958836e23010bdbb
Ishita Sequeira <ishiseq29@gmail.com>
2026-03-24 10:37:12 -0400

    fix lint failure

--- internal/controller/components/modelsasservice/modelsasservice_controller_actions_test.go
+++ internal/controller/components/modelsasservice/modelsasservice_controller_actions_test.go
@@ -718,7 +718,7 @@ func TestConfigureTelemetryPolicy(t *testing.T) {
 			}
 
 			err := configureTelemetryPolicy(t.Context(), rr)
-			g.Expect(err).ShouldNot(HaveOccurred())   // Should succeed but skip
+			g.Expect(err).ShouldNot(HaveOccurred())  // Should succeed but skip
 			g.Expect(rr.Resources).Should(BeEmpty()) // No resources should be added
 		})
 	})
