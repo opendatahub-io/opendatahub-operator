343195496afaf0d075cf5d87e129266ce77b9de0
Davide Bianchi <10374360+davidebianchi@users.noreply.github.com>
2026-06-16 15:12:15 +0200

    fix: upgrade wva sha

--- get_all_manifests.sh
+++ get_all_manifests.sh
@@ -32,7 +32,7 @@ declare -A ODH_COMPONENT_MANIFESTS=(
     ["maas"]="opendatahub-io:models-as-a-service:stable@e6203f7307b9963f952886b0cd45cc43f62f1aaf:deployment"
     ["mlflowoperator"]="opendatahub-io:mlflow-operator:main@b55bcb6aa528ccecf8904745bb0218350b1ff453:config"
     ["sparkoperator"]="opendatahub-io:spark-operator:main@1f4c2a848c928c24d914139222d24858aae6cf32:config"
-    ["wva"]="opendatahub-io:workload-variant-autoscaler:main@cd80b54f7f00748059d1c2e9a59bff4163d9833b:config"
+    ["wva"]="opendatahub-io:workload-variant-autoscaler:main@832d8fbef070af358a10955c41b4aa211ff9a499:config"
 )
 
 # RHOAI Component Manifests
@@ -54,7 +54,7 @@ declare -A RHOAI_COMPONENT_MANIFESTS=(
     ["maas"]="red-hat-data-services:maas-billing:rhoai-3.5-ea.2@93e09aaabe9ede550a786744e2f01b6fcddde959:deployment"
     ["mlflowoperator"]="red-hat-data-services:mlflow-operator:rhoai-3.5-ea.2@2adb76fe3cf7e4faf8cfc9c5435c288d029c42d8:config"
     ["sparkoperator"]="red-hat-data-services:spark-operator:rhoai-3.5-ea.2@c9e197c63f003022feefdc2405ae081ec8b1916f:config"
-    ["wva"]="red-hat-data-services:workload-variant-autoscaler:rhoai-3.5-ea.2@259243fce898b26a88b4cdf5ae579460a97e1f7b:config"
+    ["wva"]="red-hat-data-services:workload-variant-autoscaler:rhoai-3.5-ea.2@19359efbab0299a8148506d2438cfe65f47982aa:config"
 )
 
 # {ODH,RHOAI}_COMPONENT_CHARTS are lists of chart repositories info to fetch helm charts
