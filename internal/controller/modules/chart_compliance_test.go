55f48a860d0adebf7f701e9fc95d12630ba22fa4
Steven Tobin <stobin@redhat.com>
2026-07-10 15:05:42 +0100

    we need to allow some extra types for kustomize

--- internal/controller/modules/chart_compliance_test.go
+++ internal/controller/modules/chart_compliance_test.go
@@ -24,7 +24,23 @@ func dirExists(path string) bool {
 	return err == nil && info.IsDir()
 }
 
-var allowedKinds = map[string]bool{
+var helmAllowedKinds = map[string]bool{
+	"Deployment":                     true,
+	"Service":                        true,
+	"ServiceAccount":                 true,
+	"ClusterRole":                    true,
+	"ClusterRoleBinding":             true,
+	"Role":                           true,
+	"RoleBinding":                    true,
+	"ConfigMap":                      true,
+	"CustomResourceDefinition":       true,
+	"MutatingWebhookConfiguration":   true,
+	"ValidatingWebhookConfiguration": true,
+	"Issuer":                         true,
+	"Certificate":                    true,
+}
+
+var kustomizeAllowedKinds = map[string]bool{
 	"Deployment":                     true,
 	"Service":                        true,
 	"ServiceAccount":                 true,
@@ -116,7 +132,7 @@ func TestModuleManifestRendering(t *testing.T) {
 					deploymentCount := 0
 					for _, res := range resources {
 						kind := res.GetKind()
-						g.Expect(allowedKinds).Should(HaveKey(kind),
+						g.Expect(helmAllowedKinds).Should(HaveKey(kind),
 							"chart %s contains disallowed resource kind %q (name: %s)",
 							handler.GetName(), kind, res.GetName())
 
@@ -154,7 +170,7 @@ func TestModuleManifestRendering(t *testing.T) {
 					deploymentCount := 0
 					for _, res := range resources {
 						kind := res.GetKind()
-						g.Expect(allowedKinds).Should(HaveKey(kind),
+						g.Expect(kustomizeAllowedKinds).Should(HaveKey(kind),
 							"kustomize %s contains disallowed resource kind %q (name: %s)",
 							handler.GetName(), kind, res.GetName())
 
