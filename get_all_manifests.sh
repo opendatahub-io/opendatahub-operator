63ff4f77aa1bb78baac1ef3d4ea7152733b68986
Jan Stourac <jstourac@redhat.com>
2026-07-28 01:08:30 +0200

    Use a release branch for the workbenches-operator chart in rhoai

--- get_all_manifests.sh
+++ get_all_manifests.sh
@@ -85,7 +85,7 @@ declare -A RHOAI_CCM_CHARTS=(
 # RHOAI Component Charts
 declare -A RHOAI_COMPONENT_CHARTS=(
     ["dashboard-operator"]="red-hat-data-services:odh-dashboard:rhoai-3.5@483ec0c8f3f45cad3d4d185a191c36eda1cdbd47:dashboard-operator/charts/dashboard"
-    ["workbenches"]="red-hat-data-services:workbenches-operator:main@eb970ef4f20a402f87ecb4b6d3f5a3860485167c:charts/operator"
+    ["workbenches"]="red-hat-data-services:workbenches-operator:rhoai-3.5@0ee58985bf55b877987f3e3a9c442b276f121487:charts/operator"
     ["feastoperator"]="red-hat-data-services:feast-module-operator:rhoai-3.5@bb135ff9df5e2fea2b58348c9ecd2ad3c5659759:config/chart"
 )
 
