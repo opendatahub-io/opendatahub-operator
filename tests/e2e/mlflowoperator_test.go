c5fd7067b683427e918e0a2afbd23291599b944a
sduvvuri1603 <sduvvuri@redhat.com>
2026-09-07 23:59:44 -0400

    test: expand MLflowOperator ODH e2e to Feast peer depth
    Add platform-path checks for module operator Deployment readiness,
    status.releases, DSC MLflowOperatorReady, and real cleanup on Removed
    for RHOAIENG-80551 Part 2.
    
    Signed-off-by: sduvvuri1603 <sduvvuri@redhat.com>

--- tests/e2e/mlflowoperator_test.go
+++ tests/e2e/mlflowoperator_test.go
@@ -6,6 +6,7 @@ import (
 	operatorv1 "github.com/openshift/api/operator/v1"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 
 	componentApi "github.com/opendatahub-io/opendatahub-operator/v2/api/components/v1alpha1"
 	"github.com/opendatahub-io/opendatahub-operator/v2/internal/controller/status"
@@ -20,8 +21,14 @@ type MLflowOperatorTestCtx struct {
 }
 
 const (
-	mlflowValidateEnabledName  = "Validate component enabled"
-	mlflowValidateDisabledName = "Validate component disabled"
+	mlflowValidateEnabledName              = "Validate component enabled"
+	mlflowValidateModuleOperatorDeployName = "Validate module operator deployment"
+	mlflowValidateModuleReleasesName       = "Validate module releases"
+	mlflowValidateDSCReadyName             = "Validate DSC MLflowOperatorReady condition"
+	mlflowValidateDisabledName             = "Validate component disabled"
+
+	// Matches internal/controller/modules/mlflowoperator/handler.go DeploymentName.
+	mlflowModuleOperatorDeployment = "mlflow-operator-controller-manager"
 )
 
 func mlflowOperatorTestSuite(t *testing.T) {
@@ -36,6 +43,9 @@ func mlflowOperatorTestSuite(t *testing.T) {
 
 	testCases := []TestCase{
 		{mlflowValidateEnabledName, componentCtx.ValidateModuleEnabled},
+		{mlflowValidateModuleOperatorDeployName, componentCtx.ValidateModuleOperatorDeployment},
+		{mlflowValidateModuleReleasesName, componentCtx.ValidateModuleReleases},
+		{mlflowValidateDSCReadyName, componentCtx.ValidateDSCMLflowOperatorReady},
 		{mlflowValidateDisabledName, componentCtx.ValidateModuleDisabled},
 	}
 
@@ -68,12 +78,69 @@ func (tc *MLflowOperatorTestCtx) ValidateModuleEnabled(t *testing.T) {
 	)
 }
 
+func (tc *MLflowOperatorTestCtx) ValidateModuleOperatorDeployment(t *testing.T) {
+	t.Helper()
+
+	skipUnless(t, Smoke, Tier1)
+
+	tc.EnsureResourceExists(
+		WithMinimalObject(gvk.Deployment, types.NamespacedName{
+			Namespace: tc.AppsNamespace,
+			Name:      mlflowModuleOperatorDeployment,
+		}),
+		WithCondition(jq.Match(`.status.readyReplicas >= 1`)),
+		WithCustomErrorMsg("mlflow-operator-controller-manager should be ready in %s", tc.AppsNamespace),
+	)
+}
+
+func (tc *MLflowOperatorTestCtx) ValidateModuleReleases(t *testing.T) {
+	t.Helper()
+
+	tc.SkipIfXKSCluster(t)
+
+	skipUnless(t, Smoke)
+
+	tc.EnsureResourceExists(
+		WithMinimalObject(tc.GVK, tc.NamespacedName),
+		WithCondition(
+			jq.Match(`[.status.releases[]? | select(.name != "" and .version != "" and .repoUrl != "")] | length > 0`),
+		),
+		WithCustomErrorMsg("MLflowOperator CR should expose non-empty status.releases entries"),
+	)
+}
+
+func (tc *MLflowOperatorTestCtx) ValidateDSCMLflowOperatorReady(t *testing.T) {
+	t.Helper()
+
+	skipUnless(t, Smoke, Tier1)
+
+	tc.EnsureResourceExists(
+		WithMinimalObject(gvk.DataScienceCluster, tc.DataScienceClusterNamespacedName),
+		WithCondition(jq.Match(
+			`.status.conditions[] | select(.type == "%sReady") | .status == "%s"`,
+			componentApi.MLflowOperatorKind,
+			metav1.ConditionTrue,
+		)),
+		WithCustomErrorMsg("DataScienceCluster should have MLflowOperatorReady condition set to True"),
+	)
+}
+
 func (tc *MLflowOperatorTestCtx) ValidateModuleDisabled(t *testing.T) {
 	t.Helper()
 
 	skipUnless(t, Smoke, Tier1)
 
 	tc.EnsureResourcesExist(WithMinimalObject(tc.GVK, tc.NamespacedName))
+
 	tc.UpdateComponentState(operatorv1.Removed)
-	tc.EnsureResourcesGone(WithMinimalObject(tc.GVK, tc.NamespacedName))
+
+	tc.EnsureResourceGone(WithMinimalObject(tc.GVK, tc.NamespacedName))
+
+	tc.EnsureResourceGone(
+		WithMinimalObject(gvk.Deployment, types.NamespacedName{
+			Namespace: tc.AppsNamespace,
+			Name:      mlflowModuleOperatorDeployment,
+		}),
+		WithEventuallyTimeout(tc.TestTimeouts.componentReadinessTimeout),
+	)
 }
