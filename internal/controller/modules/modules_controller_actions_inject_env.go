d3189acd9c048459309d246f1020488af3810089
Heorhii Churhuliia <hchurhul@redhat.com>
2026-09-22 10:12:32 +0100

    add namespase to skipping env injection in Deployment

--- internal/controller/modules/modules_controller_actions_inject_env.go
+++ internal/controller/modules/modules_controller_actions_inject_env.go
@@ -85,7 +85,7 @@ func injectEnvVarsIntoDeployment(log logr.Logger, obj *unstructured.Unstructured
 		idx := findNamedContainer(containers, targetName)
 		if idx < 0 {
 			log.Error(nil, "target container not found in Deployment, skipping env injection",
-				"deployment", deployName, "container", targetName)
+				"deployment", deployName, "deploymentNamespace", obj.GetNamespace(), "container", targetName)
 			continue
 		}
 
