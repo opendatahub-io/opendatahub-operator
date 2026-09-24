9242f60a5f576c9264e77dc8820bd9aff193440e
Marián Macik <mmacik@redhat.com>
2026-09-24 18:55:19 +0200

    fix: deploy RHAII on xKS with update env vars aligned with modularization

--- internal/controller/modules/mlflowoperator/handler.go
+++ internal/controller/modules/mlflowoperator/handler.go
@@ -38,6 +38,7 @@ var (
 		cluster.ManagedRhoai:     rhoaiPlatformOverlay,
 		cluster.SelfManagedRhoai: rhoaiPlatformOverlay,
 		cluster.OpenDataHub:      opendatahubOverlayPath,
+		cluster.XKS:              rhoaiPlatformOverlay,
 	}
 )
 
